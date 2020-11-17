985e89a599912f926d1c81c89b9cf20b81bc2e1c
javierps <javier.perezsaez@gmail.com>
2020-11-17 10:05:52 +0100

    unique cntrd_table gidx

--- Analysis/R/prepare_map_data_revised.R
+++ Analysis/R/prepare_map_data_revised.R
@@ -179,8 +179,9 @@ DBI::dbSendStatement(
 DBI::dbSendStatement(
   conn_pg, 
   glue::glue_sql(
-    "CREATE INDEX cntrds_{`DBI::SQL(dbuser)`}_gidx on {`{DBI::SQL(cntrd_table)}`} USING GIST(geom);",
+    "CREATE INDEX {`{DBI::SQL(paste0(cntrd_table, '_gidx'))}`} on {`{DBI::SQL(cntrd_table)}`} USING GIST(geom);",
     .con = conn_pg)) 
+DBI::dbSendStatement(conn_pg, glue::glue_sql("VACUUM ANALYZE {`{DBI::SQL(cntrd_table)}`};", .con = conn_pg))
 
 # Create sf_chol ---------------------------------------------------------------
 
