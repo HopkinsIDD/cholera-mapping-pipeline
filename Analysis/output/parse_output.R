68b9f025e0d4b2e64c61eae9c31b671aa67bf20e
javierps <javier.perezsaez@gmail.com>
2021-02-01 18:54:03 +0100

    add WHO annual cases

--- Analysis/output/parse_output.R
+++ Analysis/output/parse_output.R
@@ -44,18 +44,18 @@ dir.create(out_dir, recursive = T)
 
 # What country are we running?
 country <- str_extract(opt$config, "[A-Z]{3}")
-  
+
 # Prepare out file names for each output type
 obs_stats_filename <- str_c(out_dir, "/", country, "_obs_stats.csv")
 runtimes_filename <- str_c(out_dir, "/", country, "_model_runtimes.csv")
 case_rast_filename <- str_c(out_dir, "/", country, "_case_rast.rds")
 case_comp_filename <- str_c(out_dir, "/", country, "_case_comp.csv")
 rhat_filename <- str_c(out_dir, "/", country, "_case_rhats.csv")
+who_filename <- str_c(out_dir, "/", country, "_who_comp.csv")
 
 # Get observations --------------------------------------------------------
 sf_cases <- taxdat::read_file_of_type(preprocessed_data_filename, "sf_cases")
 sf_cases_resized <- taxdat::read_file_of_type(stan_input_filename, "stan_input")$sf_cases_resized
-
 
 # Get stan object
 model.rand <- taxdat::read_file_of_type(stan_output_filename, "model.rand")
@@ -74,46 +74,72 @@ obs_stats <- taxdat::get_obs_stats(sf_cases_resized)
 write_csv(obs_stats %>% mutate(country = country), path = obs_stats_filename)
 
 # Extract the case raster -------------------------------------------------
-case_raster <- taxdat::get_case_raster(preprocessed_data_filename = file_names["data"],
-                                       covar_data_filename = file_names["covar"],
-                                       model_output_filenames = file_names["stan_output"])
-
-colnames(case_raster)[8:9] <- c("modeled cases", "modeled rates")
-
-# Write to file
-saveRDS(case_raster %>% mutate(country = country), file = case_rast_filename)
+if (!file.exists(case_rast_filename)) {
+  case_raster <- taxdat::get_case_raster(preprocessed_data_filename = file_names["data"],
+                                         covar_data_filename = file_names["covar"],
+                                         model_output_filenames = file_names["stan_output"])
+  
+  colnames(case_raster)[8:9] <- c("modeled cases", "modeled rates")
+  
+  # Write to file
+  saveRDS(case_raster %>% mutate(country = country), file = case_rast_filename)
+}
 
 # Cases vs. modeled ---------------------------------------------------------
-# Extract comparison between modeled an observed cases
-data_fidelity <- taxdat::get_data_fidelity(stan_output_filename)[[1]] %>% 
-  as_tibble()
 
-write_csv(data_fidelity %>% mutate(country = country) %>% set_colnames(c("chain", "param", "modeled", "actual", "country")), path = case_comp_filename)
+if (!file.exists(case_comp_filename)) {
+  # Extract comparison between modeled an observed cases
+  data_fidelity <- taxdat::get_data_fidelity(stan_output_filename)[[1]] %>% 
+    as_tibble()
+  
+  write_csv(data_fidelity %>% mutate(country = country) %>% 
+              set_colnames(c("chain", "param", "modeled", "actual", "country")), 
+            path = case_comp_filename)
+}
 
 # Rhats -------------------------------------------------------------------
-# Extract the Rhats for modeled cases
-rhat_obs <- rstan::summary(model.rand, pars = "modeled_cases")$summary %>% 
-  as_tibble() %>% 
-  dplyr::select(mean, Rhat) %>% 
-  mutate(obs = row_number()) %>% 
-  # Join with the information on the data
-  inner_join(
-    aggregated_data <- tibble(
-      obs = seq_along(stan_input$stan_data$y),
-      y = stan_input$stan_data$y,
-      censoring = stan_input$stan_data$censoring_inds
-    ) %>%  
-      inner_join(
-        tibble(
-          obs = stan_input$stan_data$map_obs_loctime_obs,
-          lp = stan_input$stan_data$u_loctime[stan_input$stan_data$map_obs_loctime_loc],
-          tfrac = stan_input$stan_data$tfrac
-        )
-      ) %>%
-      inner_join(covar_cube_output$location_periods_dict %>%
-                   group_by(location_period_id, loctime_id, t) %>%
-                   summarise(n_cell = n()),
-                 by = c("lp" = "loctime_id")) %>%
-      mutate(location_period_id = as.character(location_period_id)))
 
-write_csv(rhat_obs %>% mutate(country = country), path = rhat_filename)
+if (!file.exists(rhat_filename)) {
+  # Extract the Rhats for modeled cases
+  rhat_obs <- rstan::summary(model.rand, pars = "modeled_cases")$summary %>% 
+    as_tibble() %>% 
+    dplyr::select(mean, Rhat) %>% 
+    mutate(obs = row_number()) %>% 
+    # Join with the information on the data
+    inner_join(
+      aggregated_data <- tibble(
+        obs = seq_along(stan_input$stan_data$y),
+        y = stan_input$stan_data$y,
+        censoring = stan_input$stan_data$censoring_inds
+      ) %>%  
+        inner_join(
+          tibble(
+            obs = stan_input$stan_data$map_obs_loctime_obs,
+            lp = stan_input$stan_data$u_loctime[stan_input$stan_data$map_obs_loctime_loc],
+            tfrac = stan_input$stan_data$tfrac
+          )
+        ) %>%
+        inner_join(covar_cube_output$location_periods_dict %>%
+                     group_by(location_period_id, loctime_id, t) %>%
+                     summarise(n_cell = n()),
+                   by = c("lp" = "loctime_id")) %>%
+        mutate(location_period_id = as.character(location_period_id)))
+  write_csv(rhat_obs %>% mutate(country = country), path = rhat_filename)
+}
+
+
+# WHO cases ---------------------------------------------------------------
+if (!file.exists(who_filename)) {
+  who_annual_cases <- sf_cases_resized
+  chains <- rstan::extract(model.rand)
+  who_annual_cases$modeled <- apply(chains$modeled_cases, 2, mean)
+  who_annual_cases$observed <- who_annual_cases$attributes.fields.suspected_cases # fix me
+  who_annual_cases <- taxdat::pull_output_by_source(who_annual_cases, "%WHO Annual Cholera Reports%",
+                                                    database_api_key_rfile = str_c(opt$cholera_directory,
+                                                                                   "Analysis/R/database_api_key.R"))
+  who_annual_cases %>%
+    as.data.frame() %>%
+    dplyr::select(OC_UID, TL, TR, observed, modeled) %>% 
+    write_csv(path = who_filename)
+}
+
