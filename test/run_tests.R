aba6c9fc55d1a7cecd6ed024c63d1485e0b3eb99
jkamins7 <jkaminsky@jhu.edu>
2021-04-12 12:23:26 -0400

    Testing now creates data folder if not exists

--- test/run_tests.R
+++ test/run_tests.R
@@ -1,6 +1,9 @@
 options(error = function(...) {
   quit(..., status = 2)
 })
+if (!dir.exists("../Analysis/data")) {
+  dir.create("../Analysis/data")
+}
 print("Running tests")
 install.packages("../packages/taxdat", type = "source", repos = NULL)
 source("../Analysis/R/set_parameters.R")
