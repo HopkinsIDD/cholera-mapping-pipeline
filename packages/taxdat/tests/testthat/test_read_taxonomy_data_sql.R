656f97e80dfcf1125768cbd4e38dc82ed040041e
QLLZ <44678888+QLLZ@users.noreply.github.com>
2021-07-01 16:31:26 -0400

    Create test_read_taxonomy_data_sql.R

---
+++ packages/taxdat/tests/testthat/test_read_taxonomy_data_sql.R
@@ -0,0 +1,63 @@
+username=Sys.getenv("username")
+password=Sys.getenv("password")
+library(testthat)
+#test read_taxonomy_data_sql function
+test_that("read_taxonomy_data_sql works",{
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=NULL,password=password,locations=14)
+    )
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=NULL,locations=14)
+    )
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=NULL)
+    )
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations="AFR::ETH")
+    )
+  expect_warning(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=NULL,time_right=NULL,uids=NULL)
+    )
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left=as.Date("2019-06-01"),time_right=as.Date("2019-01-01"))
+    )
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left=NULL,time_right=NULL,uids=301)
+    )
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=as.Date("3000-01-01"),time_right=NULL,uids=301)
+    )
+  
+  #expect extract the correct data
+  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=NULL,time_right=NULL,uids=301)
+  expect_false(
+    any(duplicated(taxonomy_data))
+  )
+  expect_equal(
+    taxonomy_data$observation_collection_id,301
+  )
+  expect_equal(
+    taxonomy_data$location_id,2
+  )
+  
+  # expect extract the correct data for multiple locations
+  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=c(28365,28366,28369),time_left=NULL,time_right=NULL,uids=NULL)
+  expect_false(
+    any(duplicated(taxonomy_data))
+  )
+  expect_true(
+    all(unique(taxonomy_data$location_id)%in%c(28365,28366,28369))
+  )
+
+  # expect extract the correct data for multiple observation collections
+  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=NULL,time_right=NULL,uids=c(301,812,3540))
+  expect_false(
+    any(duplicated(taxonomy_data))
+  )
+  expect_true(
+    all(unique(taxonomy_data$observation_collection_id)%in%c(301,812,3540))
+  )
+  expect_true(
+    unique(taxonomy_data$location_id)==2
+  )
+})
