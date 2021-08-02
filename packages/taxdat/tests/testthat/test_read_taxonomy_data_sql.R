4ace3c30646278253a6d7b50ac17b3c1f0292413
QLLZ <44678888+QLLZ@users.noreply.github.com>
2021-08-02 15:11:47 -0400

    Update test_read_taxonomy_data_sql.R

--- packages/taxdat/tests/testthat/test_read_taxonomy_data_sql.R
+++ packages/taxdat/tests/testthat/test_read_taxonomy_data_sql.R
@@ -1,85 +1,78 @@
-username=Sys.getenv("username")
-password=Sys.getenv("password")
+dbuser <- Sys.getenv("USER", "app")
+dbname <- Sys.getenv("CHOLERA_COVAR_DBNAME", "cholera_covariates")
 
 library(testthat)
 #test read_taxonomy_data_sql function
 test_that("read_taxonomy_data_sql works",{
+  #1. Missing arguments
+  ## missing user name
   expect_error(
     taxonomy_data=read_taxonomy_data_sql(username=NULL,password=password,locations=14)
     )
+  ## missing password
   expect_error(
     taxonomy_data=read_taxonomy_data_sql(username=username,password=NULL,locations=14)
     )
+  ## missing locations (for now, locations can't be null)
   expect_error(
-    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=NULL)
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=NULL,uids=314)#This OC has data from WHO annual reports.
     )
+  #2. Arguments with invalid formats
+  ## locations have to be numbers (unique location ids)
   expect_error(
     taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations="AFR::ETH")
     )
+  ## time_left and time_right have to be date format
   expect_error(
-    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left=as.Date("2019-06-01"),time_right=as.Date("2019-01-01"))
-    )
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left="200-01-01",time_right="2000-12-31")
+  )
+  ## uids have to be integer. (unique identifiers of observation collections)
   expect_error(
-    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left=NULL,time_right=NULL,uids=301)
-    )
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,uids=31.4)
+  )  
+  # 3. Arguments with values exceeding the valid ranges. 
+  ## time_left is later than time_right
   expect_error(
-    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=as.Date("3000-01-01"),time_right=NULL,uids=301)
-    )
-  
-  #expect extract the correct data
-  skip("Skip the following tests which require pulling data from the server until testing database is ready.")
-  
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left="2000-01-01",time_right="1999-01-01")
+  )  
+  ## uids <0
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,uids=-314)
+  )
+  ## Locations is null
+  expect_error(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=NULL,uids=314)
+  )  
+  # 4. All locations are either the locations specified in the arguments or their child locations. 
+  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left=NULL,time_right=NULL,uids=21136)
+  #expect all locations are correct under Ethiopia
+  expect_true(
+    all(stringr::str_detect(taxonomy_data$location_name,"AFR::ETH"))
+  )
+  # 5. All dates are within the range of time_left and time_right
+  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,time_left="2019-01-01",time_right="2020-08-02",uids=21136)
+  expect_true(
+    all(taxonomy_data$TL>=as.Date("2019-01-01")&(taxonomy_data$TR<=as.Date("2020-08-02")))
+  )
+  # 6. All uids are within the uids 
+  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,uids=c(314,21041,21136))
+  expect_true(
+    any(taxonomy_data$observation_collection_id==314)&any(taxonomy_data$observation_collection_id==21041)&any(taxonomy_data$observation_collection_id==21136)
+  ) 
+  # 7. No duplicate data
+  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14,uids=c(314,21041,21136))
+  expect_false(
+    any(duplicated(taxonomy_data))
+  )
+  # 8. Memory warnings: tell users that they are pulling all the data for a region/time period
+  ## when time_right/time_left are null, they are pulling all the data for a region (including data linked to its child locations)
   expect_warning(
-    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=NULL,time_right=NULL,uids=NULL),
-    "No filters specified on data pull, pulling all data."
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=14),
+    "No time filters."
   )
-  
-  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=NULL,time_right=NULL,uids=301) #location 2 is Burund.
-  
-  expect_false(
-    any(duplicated(taxonomy_data))
-  )
-  expect_equal(
-    taxonomy_data$observation_collection_id,301
-  )
-  expect_equal(
-    taxonomy_data$location_id,2
-  )
-  
-  # expect extract the correct data for multiple locations
-  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=c(28365,28366,28369),time_left=NULL,time_right=NULL,uids=NULL)
-  expect_false(
-    any(duplicated(taxonomy_data))
-  )
-  expect_true(
-    all(unique(taxonomy_data$location_id)%in%c(28365,28366,28369))
-  )
-
-  # expect extract the correct data for multiple observation collections
-  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=2,time_left=NULL,time_right=NULL,uids=c(301,812,3540))
-  expect_false(
-    any(duplicated(taxonomy_data))
-  )
-  expect_true(
-    all(unique(taxonomy_data$observation_collection_id)%in%c(301,812,3540))
-  )
-  expect_true(
-    unique(taxonomy_data$location_id)==2
-  )
-  
-  #expect the time format is correct and falls in the correct time range
-  taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=405,time_left=NULL,time_right=NULL,uids=c(20378))#location 405 is Kenya
-  expect_true(
-    min(as.Date(taxonomy_data$TL))==as.Date("2012-01-26")
-  )
-  expect_true(
-    max(as.Date(taxonomy_data$TR))==as.Date("2012-12-09")
-  )
-  expect_true(
-    all(taxonomy_data$TR>=axonomy_data$TL)
-  )
-  #expect all locations are correct under Kenya
-  expect_true(
-    all(stringr::str_detect(taxonomy_data$location_name,"AFR::KEN"))
+  ## when locations are null, they are pulling all the data for a time period (including data for its child locations)
+  expect_warning(
+    taxonomy_data=read_taxonomy_data_sql(username=username,password=password,location=NULL,time_left="2000-01-01",time_right="2000-12-31"),
+    "No location filters."
   )
 })
