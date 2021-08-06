af07ffc0fcf041bd4abd077fc4cbefb2b9758345
QLLZ <44678888+QLLZ@users.noreply.github.com>
2021-08-06 12:50:30 -0400

    Update test_read_taxonomy_data_sql.R

--- packages/taxdat/tests/testthat/test_read_taxonomy_data_sql.R
+++ packages/taxdat/tests/testthat/test_read_taxonomy_data_sql.R
@@ -87,8 +87,9 @@ test_that("read_taxonomy_data_sql works",{
   
   ## when locations are null, they are pulling all the data for a time period (including data for its child locations)
   #testthat::skip_on_ci()
-  expect_warning(taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=NULL,time_left=as.Date("2000-01-01"),time_right=as.Date("2000-01-31")))
+  expect_warning(taxonomy_data=read_taxonomy_data_sql(username=username,password=password,locations=NULL,time_left=as.Date("2000-01-01"),time_right=as.Date("2000-01-31")),"No location filters")
+  
   ## when locations and TL/TR are null, they are pulling all the data for all time periods (including data for its child locations)
   #testthat::skip_on_ci()
-  expect_warning(taxonomy_data=read_taxonomy_data_sql(username=username,password=password))
+  expect_warning(taxonomy_data=read_taxonomy_data_sql(username=username,password=password),"No filters")
 })
