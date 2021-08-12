e73303668257a5749cc72a39fd75ffbea8788d3e
QLLZ <44678888+QLLZ@users.noreply.github.com>
2021-08-12 10:33:44 -0400

    Update run_test_OverlappingGrid_qulu.R

--- Analysis/R/test_case/Grid/run_test_OverlappingGrid_qulu.R
+++ Analysis/R/test_case/Grid/run_test_OverlappingGrid_qulu.R
@@ -7,11 +7,13 @@ dbname <- Sys.getenv("CHOLERA_COVAR_DBNAME", "cholera_covariates")
 conn_pg <- taxdat::connect_to_db(dbuser,dbname)
 DBI::dbClearResult(DBI::dbSendQuery(conn = conn_pg, "SET client_min_messages TO WARNING;"))
 
+query_time_left <- lubridate::ymd("2000-01-01")
+query_time_right <- lubridate::ymd("2000-12-31")
 ## Pull data frames needed to create testing database from the api This doesn't
 ## pull covariates, but does pull everything else
 all_dfs <- taxdat::create_testing_dfs_from_api(username = Sys.getenv("CHOLERA_API_USERNAME"), 
-                                               api_key = Sys.getenv("CHOLERA_API_KEY"), locations = "AFR::KEN", time_left = lubridate::ymd("2000-01-01"), 
-                                               time_right = lubridate::ymd("2000-12-31"), uids = NULL, website = "https://api.cholera-taxonomy.middle-distance.com/")
+                                               api_key = Sys.getenv("CHOLERA_API_KEY"), locations = "AFR::KEN", time_left = query_time_left, 
+                                               time_right = query_time_right, uids = NULL, website = "https://api.cholera-taxonomy.middle-distance.com/")
 
 ## ------------------------------------------------------------------------------------------------------------------------
 ## Change polygons
@@ -40,59 +42,23 @@ all_dfs$location_df <- all_dfs$shapes_df %>%
 test_extent <- sf::st_bbox(all_dfs$shapes_df)
 test_raster <- create_test_raster(nrows = 10, ncols = 10, nlayers = 2, test_extent)
 test_covariates <- create_multiple_test_covariates(test_raster = test_raster)
-test_covariates[[1]]$covariate <- 1 + 10^test_covariates[[1]][["covariate"]]
-
-min_time_left <- min(all_dfs$observations_df$time_left)
-max_time_right <- max(all_dfs$observations_df$time_right)
-covariate_raster_funs <- lapply(seq_len(length(test_covariates)), function(covariate_idx) {
-  covariate <- test_covariates[[covariate_idx]]
-  min_time_index <- min(covariate$t)
-  max_time_index <- max(covariate$t)
-  lapply(unique(covariate$t), function(time_index) {
-    return(list(name = ifelse(covariate_idx == 1, "population", paste("covariate",
-                                                                      covariate_idx, sep = "")), start_date = min_time_left + ((time_index -
-                                                                                                                                  1) - min_time_index)/(max_time_index - min_time_index) * (max_time_right -
-                                                                                                                                                                                              min_time_left), end_date = min_time_left + (time_index - min_time_index)/(max_time_index -
-                                                                                                                                                                                                                                                                          min_time_index) * (max_time_right - min_time_left), fun = function(psql_connection) {
-                                                                                                                                                                                                                                                                            covariate %>%
-                                                                                                                                                                                                                                                                              dplyr::filter(t == time_index) %>%
-                                                                                                                                                                                                                                                                              dplyr::select(covariate) %>%
-                                                                                                                                                                                                                                                                              stars::st_rasterize(nx = max(test_raster$row), ny = max(test_raster$col)) %>%
-                                                                                                                                                                                                                                                                              stars:::st_as_raster() %>%
-                                                                                                                                                                                                                                                                              return()
-                                                                                                                                                                                                                                                                          }))
-  })
-}) %>%
-  unlist(recursive = FALSE)
+min_time_left <- query_time_left
+max_time_right <- query_time_right
+covariate_raster_funs <- taxdat:::convert_simulated_covariates_to_test_covariate_funs(test_covariates, 
+                                                                                      min_time_left, max_time_right)
 
 ## ------------------------------------------------------------------------------------------------------------------------
 ## Change observations
-raster_df <- lapply(covariate_raster_funs, function(x) {
-  tibble::tibble(name = x$name, start_date = x$start_date, end_date = x$end_date,
-                 covar = list(x$fun()))
-}) %>%
-  do.call(what = bind_rows) %>%
-  dplyr::group_by(name) %>%
-  summarize(covar = list(do.call(what = bind_rows, mapply(SIMPLIFY = FALSE, covar,
-                                                          start_date, FUN = function(covar, time_left) {
-                                                            tmp <- reshape2::melt(array(raster::values(covar), dim(covar[[1]])))
-                                                            tmp$geometry <- sf::st_geometry(sf::st_as_sf(raster::rasterToPolygons(covar)))
-                                                            tmp$id <- seq_len(nrow(tmp))
-                                                            tmp$row <- tmp$Var1
-                                                            tmp$col <- tmp$Var2
-                                                            tmp$t <- which(start_date == time_left)
-                                                            tmp$covariate <- tmp$value
-                                                            return(sf::st_as_sf(tmp[, c("id", "row", "col", "t", "covariate", "geometry")]))
-                                                          }))), .groups = "drop") %>%
-  dplyr::arrange(as.numeric(gsub("covariate", "", gsub("population", "0", name))))
+raster_df <- taxdat::convert_test_covariate_funs_to_simulation_covariates(covariate_raster_funs)
 
-raster_df$covar[[1]]$covariate <- log(raster_df$covar[[1]]$covariate - 1)/log(10)
-
-test_underlying_distribution <- create_underlying_distribution(covariates = raster_df$covar)
+test_underlying_distribution <- create_underlying_distribution(covariates = raster_df)
 
 test_observations <- observe_polygons(test_polygons = dplyr::mutate(all_dfs$shapes_df,
                                                                     location = qualified_name, geometry = geom), test_covariates = raster_df$covar,
-                                      underlying_distribution = test_underlying_distribution, noise = FALSE)
+                                      underlying_distribution = test_underlying_distribution, 
+                                      min_time_left = query_time_left, 
+                                      max_time_right = query_time_right,
+                                      noise = FALSE)
 
 #
 all_dfs$observations_df <- test_observations %>%
