902df171d07e8322a9071729056ad4a340ad61a0
QLLZ <44678888+QLLZ@users.noreply.github.com>
2021-09-22 16:31:29 -0400

    Update run_test_gridded.R

--- Analysis/R/run_test_gridded.R
+++ Analysis/R/run_test_gridded.R
@@ -96,7 +96,8 @@ my_seed <- c(10403, 624, 105045778, 1207077739, 2042172336, -219892751, -7680601
     671520922, -73267885, -1068074376, -470799847, -477234426, 1769427407, -712833596, 
     -1493528075, 33903154, 899767307, 2146321360, -821174895, 1609288222, -1825415161, 
     -834781028, -1849841427, -10400054, -1061791293, -296237016, 429740041, -804090826, 
-    472843583, -97884556, -509874459)
+    472843583, -97884556, -509874459) %>%
+    as.integer()
 
 
 query_time_left <- lubridate::ymd("2000-01-01")
@@ -115,7 +116,7 @@ load(rprojroot::find_root_file(criterion = ".choldir", "Analysis", "all_dfs_obje
 ## ------------------------------------------------------------------------------------------------------------------------
 ## Change polygons
 test_extent <- sf::st_bbox(all_dfs$shapes_df)
-test_raster <- create_test_raster(nrows = 10, ncols = 10, nlayers = 1, test_extent)
+test_raster <- create_test_raster(nrows = 10, ncols = 10, nlayers = 1, test_extent = test_extent)
 # Create 3 layers of testing polygons starting with a single country, and
 # splitting each polygon into 4 sub-polygons
 test_polygons <- sf::st_make_valid(create_test_layered_polygons(test_raster = test_raster, 
@@ -136,10 +137,11 @@ all_dfs$location_df <- all_dfs$shapes_df %>%
     dplyr::group_by(qualified_name) %>%
     dplyr::summarize()
 
+
 ## ------------------------------------------------------------------------------------------------------------------------
 ## Change covariates
 test_extent <- sf::st_bbox(all_dfs$shapes_df)
-test_raster <- create_test_raster(nrows = 10, ncols = 10, nlayers = 1, test_extent)
+test_raster <- create_test_raster(nrows = 10, ncols = 10, nlayers = 1, test_extent = test_extent)
 test_covariates <- create_multiple_test_covariates(test_raster = test_raster, ncovariates = 2, 
     nonspatial = c(FALSE, FALSE), nontemporal = c(FALSE, FALSE), spatially_smooth = c(TRUE, 
         FALSE), temporally_smooth = c(FALSE, FALSE), polygonal = c(TRUE, TRUE), radiating = c(FALSE, 
@@ -158,26 +160,17 @@ test_underlying_distribution <- create_underlying_distribution(covariates = rast
     seed = my_seed)
 my_seed <- .GlobalEnv$.Random.seed
 
-<<<<<<< HEAD
-test_polygons <- dplyr::mutate(all_dfs$shapes_df, location = qualified_name, geometry = geom)
-sf::st_crs(test_polygons)<-sf::st_crs(raster_df[[1]])
-test_observations <- observe_polygons(test_polygons = test_polygons, test_covariates = raster_df$covar, 
-                                      underlying_distribution = test_underlying_distribution, noise = FALSE, number_draws = 1, 
-                                      grid_proportion_observed = 1, polygon_proportion_observed = 1, min_time_left = query_time_left, 
-                                      max_time_right = query_time_right)
-=======
 test_observations <- observe_polygons(test_polygons = dplyr::mutate(all_dfs$shapes_df, 
-    location = qualified_name, geometry = geom), test_covariates = raster_df$covar, 
-    underlying_distribution = test_underlying_distribution, noise = FALSE, number_draws = 1, 
-    grid_proportion_observed = 1, polygon_proportion_observed = 1, min_time_left = query_time_left, 
-    max_time_right = query_time_right, seed = my_seed)
+    location = qualified_name, geometry = geom), test_covariates = raster_df, underlying_distribution = test_underlying_distribution, 
+    noise = FALSE, number_draws = 1, grid_proportion_observed = 1, polygon_proportion_observed = 1, 
+    min_time_left = query_time_left, max_time_right = query_time_right, seed = my_seed)
 my_seed <- .GlobalEnv$.Random.seed
->>>>>>> origin/dev_jk
 
 all_dfs$observations_df <- test_observations %>%
     dplyr::mutate(observation_collection_id = draw, time_left = time_left, time_right = time_right, 
-                  qualified_name = location, primary = TRUE, phantom = FALSE, suspected_cases = cases, 
-                  deaths = NA, confirmed_cases = NA)
+        qualified_name = location, primary = TRUE, phantom = FALSE, suspected_cases = cases, 
+        deaths = NA, confirmed_cases = NA)
+
 
 ## ------------------------------------------------------------------------------------------------------------------------
 ## Create Database
