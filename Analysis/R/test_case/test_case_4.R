0744231c9c83d7391d8cadcc344c9be63d58d3bf
QLLZ <44678888+QLLZ@users.noreply.github.com>
2021-11-01 13:04:46 -0400

    Update test_case_4.R

--- Analysis/R/test_case/test_case_4.R
+++ Analysis/R/test_case/test_case_4.R
@@ -232,14 +232,18 @@ config <- list(general = list(location_name = all_dfs$location_df$qualified_name
                polygon_type="Fake polygon",
                grid_coverage_type="100%",
                randomize=TRUE,
-               ncovariates=2, 
-               nonspatial = c(FALSE, FALSE), 
-               nontemporal = c(FALSE, FALSE), 
-               spatially_smooth = c(TRUE, FALSE), 
-               temporally_smooth = c(FALSE,FALSE), 
-               polygonal = c(TRUE, TRUE), 
-               radiating = c(FALSE, FALSE),
-               iteration=10000
+               ncovariates=2,
+               single_year_run=FALSE,
+               iteration=10000,
+               nonspatial = c(FALSE, FALSE,FALSE), 
+               nontemporal = c(FALSE, FALSE,FALSE), 
+               spatially_smooth = c(TRUE, TRUE,FALSE), 
+               temporally_smooth = c(FALSE,FALSE,FALSE), 
+               polygonal = c(TRUE, TRUE,TRUE), 
+               radiating = c(FALSE, FALSE,TRUE),
+               constant=c(TRUE,FALSE,FALSE),
+               Data_simulation_covariates=c(TRUE,TRUE,TRUE),
+               Model_covariates=c(TRUE,TRUE,FALSE)
 )
 
 yaml::write_yaml(x = config, file = config_filename)
@@ -257,13 +261,17 @@ rmarkdown::render(rprojroot::find_root_file(criterion = ".choldir", "Analysis", 
                                 polygon_type="Fake polygon",
                                 grid_coverage_type="100%",
                                 randomize=TRUE,
-                                ncovariates=2, 
-                                nonspatial = c(FALSE, FALSE), 
-                                nontemporal = c(FALSE, FALSE), 
-                                spatially_smooth = c(TRUE, FALSE), 
-                                temporally_smooth = c(FALSE,FALSE), 
-                                polygonal = c(TRUE, TRUE), 
-                                radiating = c(FALSE, FALSE),
-                                iteration=10000))
+                                ncovariates=2,
+                                single_year_run=FALSE,
+                                iteration=10000,
+                                nonspatial = c(FALSE, FALSE,FALSE), 
+                                nontemporal = c(FALSE, FALSE,FALSE), 
+                                spatially_smooth = c(TRUE, TRUE,FALSE), 
+                                temporally_smooth = c(FALSE,FALSE,FALSE), 
+                                polygonal = c(TRUE, TRUE,TRUE), 
+                                radiating = c(FALSE, FALSE,TRUE),
+                                constant=c(TRUE,FALSE,FALSE),
+                                Data_simulation_covariates=c(TRUE,TRUE,TRUE),
+                                Model_covariates=c(TRUE,TRUE,FALSE)))
 
 ## Actually do something with the groundtruth and output
