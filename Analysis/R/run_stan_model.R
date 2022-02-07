8501d99651e3b6f0b5189dbb16b4ec9ec3bf7699
javierps <javier.perezsaez@gmail.com>
2022-02-07 10:25:13 +0100

    use cmdstanr to fit

--- Analysis/R/run_stan_model.R
+++ Analysis/R/run_stan_model.R
@@ -12,20 +12,34 @@
 print("*** STARTING STAN MODEL ***")
 
 library(rstan)
+library(cmdstanr)
 
 # Run model ---------------------------------------------------------------
-model.rand <- rstan::stan(
-  file = stan_model_path,
+
+# Select model based on config and run level
+model <- cmdstan_model(stan_model_path,
+                       quiet = FALSE,
+                       force_recompile = F)
+
+# Correct to avoid passing strings
+initial_values_data$stan_data$censoring_inds <- NULL
+
+cmdstan_fit <- model$sample(
   data = initial_values_data$stan_data,
+  seed = 1234,
+  init =  initial_values_data$init.list,
+  sig_figs = 5,
   chains = nchain,
-  iter = niter,
-  pars = c("b", "t_rowsum", "vec_var"),
-  include = FALSE,
-  control = list(
-    max_treedepth = 15
-  ),
-  init = initial_values_data$init.list
-)
+  parallel_chains = nchain,
+  threads_per_chain = NULL,
+  iter_warmup = round(niter/2),
+  iter_sampling = round(niter/2),
+  max_treedepth = 15,
+  save_warmup = F,
+  refresh = 100,
+  show_messages = F)
+
+model.rand <- rstan::read_stan_csv(cmdstan_fit$output_files())
 
 # Save output
 save(model.rand, file = file_names[["stan_output"]])
