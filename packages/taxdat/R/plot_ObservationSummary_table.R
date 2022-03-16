273a6f46e676684665532aae93578ff67305b66f
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-03-16 12:52:17 -0400

    Update plot_ObservationSummary_table.R

--- packages/taxdat/R/plot_ObservationSummary_table.R
+++ packages/taxdat/R/plot_ObservationSummary_table.R
@@ -1,21 +1,30 @@
 #' @include plot_cache_function.R
 
-#' @name get_sf_cases_resized_no_cache
-#' @description load stan output
-#' @param config 
-#' @param cache 
-#' @return covar cube
-get_sf_cases_resized_no_cache <- function(config, cache, cholera_directory) {
-  config <- yaml::read_yaml(config_filename)
-  file_names <- taxdat::get_filenames(config, cholera_directory)
-  sf_cases_resized <- taxdat::read_file_of_type(file_names[["stan_input"]], "sf_cases_resized")
+#' @name get_stan_input_no_cache
+#' @title get_stan_input_no_cache
+#' @description load stan_input object based on the config file
+#' @param config config file that contains the parameter information
+#' @param cache the cached environment that contains all the parameter information
+#' @return stan_input object
+get_stan_input_no_cache <- function(config, cache, cholera_directory) {
+  load(config[["file_names"]][["stan_input"]])
   require(bit64)
   require(sf)
-  return(sf_cases_resized)
+  return(stan_input)
 }
-# cache the results
-get_sf_cases_resized <- cache_fun_results(name = "sf_cases_resized", fun = get_sf_cases_resized_no_cache,
-                                          overwrite = T, config = config)
+get_stan_input <- cache_fun_results("stan_input", get_stan_input_no_cache)
+
+#' @name get_sf_cases_resized_no_cache
+#' @title get_sf_cases_resized_no_cache
+#' @description load sf object (i.e.,sf_cases_resized) from stan input based on the config file
+#' @param config config file that contains the parameter information
+#' @param cache the cached environment that contains all the parameter information
+#' @return sf_cases_resized object
+get_sf_cases_resized_no_cache <- function(config, cache, cholera_directory) {
+  get_stan_input(config, cache, cholera_direcotry)
+  return(cache[["stan_input"]][["sf_cases_resized"]])
+}
+get_sf_cases_resized <- cache_fun_results("sf_cases_resized", get_sf_cases_resized_no_cache)
 
 #' @name plot_ObservationSummary_table
 #' @title plot_ObservationSummary_table
