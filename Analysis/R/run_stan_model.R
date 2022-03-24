db21fad2c85b365c34fb3c31af604335c81c421b
javierps <javier.perezsaez@gmail.com>
2022-03-24 10:10:20 +0100

    first pass using cmdstanr

--- Analysis/R/run_stan_model.R
+++ Analysis/R/run_stan_model.R
@@ -11,21 +11,31 @@
 # Stan modeling section
 print("*** STARTING STAN MODEL ***")
 
+library(cmdstanr)
 library(rstan)
 
 # Run model ---------------------------------------------------------------
-model.rand <- rstan::stan(
-  file = stan_model_path,
+
+# Compile cmdstanr model
+chol_model <- cmdstanr::cmdstan_model(stan_model_path,
+                                      quiet = FALSE,
+                                      force_recompile = F)
+
+cmdstan_fit <- chol_model$sample(
+  seed = 1234,
   data = initial_values_data$stan_data,
   chains = nchain,
-  iter = niter,
-  pars = c("b", "t_rowsum", "vec_var"),
-  include = FALSE,
-  control = list(
-    max_treedepth = 15
-  ),
-  init = initial_values_data$init.list
+  parallel_chains = chains,
+  iter_warmup = round(niter/2),
+  iter_sampling = round(niter/2),
+  max_treedepth = 15,
+  init = initial_values_data$init.list,
+  sig_figs = 5,
+  save_warmup = F
 )
+
+# Transform back to stanfit object
+model.rand <- rstan::read_stan_csv(cmdstan_fit$output_files())
 
 # Save output
 save(model.rand, file = file_names[["stan_output"]])
