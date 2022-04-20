eb5882e4096292d37b9e273d2553dfd4d4843f09
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-04-20 16:59:24 -0400

    Update plot_time_varying_pop_raster.R

--- packages/taxdat/R/plot_time_varying_pop_raster.R
+++ packages/taxdat/R/plot_time_varying_pop_raster.R
@@ -6,7 +6,7 @@
 #' @param cache 
 #' @return covar cube
 get_covar_no_cache <- function(config, cache, cholera_directory) {
-  config <- yaml::read_yaml(config_filename)
+  config <- yaml::read_yaml(config)
   file_names <- taxdat::get_filenames(config, cholera_directory)
   covar_cube_output <- taxdat::read_file_of_type(file_names["covar"], "covar_cube_output")
   require(bit64)
