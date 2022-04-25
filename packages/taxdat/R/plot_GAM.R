99e017e77db730bd79109f12dc8e0914b6cb8ec9
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-04-25 16:30:54 -0400

    Create plot_GAM.R

---
+++ packages/taxdat/R/plot_GAM.R
@@ -0,0 +1,72 @@
+#' @include plot_cache_function.R
+
+#' @name plot_gam_fit_input_cases
+#' @title plot_gam_fit_input_cases
+#' @description plot the rasters with gam fitted input cases
+#' @name name the name of the dataset (initial values)
+#' @name cache the cache environment
+#' @return ggplot object
+plot_gam_fit_input_cases <- function(name="initial_values_data",cache) {
+   plot<-cache[["initial_values_data"]]$gam_fit_input %>%
+    dplyr::group_by(sx, sy) %>%
+    dplyr::summarize(y = mean(y)) %>%
+    ggplot() +
+    geom_tile(aes(x = sx, y = sy, fill = y)) +
+    taxdat::color_scale(type = "cases", use_case = "ggplot map", use_log = FALSE)+
+    ggplot2::labs(fill="Cases")
+  return(plot)
+}
+
+
+#' @name plot_gam_fit_input_rates
+#' @title plot_gam_fit_input_rates
+#' @description plot the rasters with gam fitted input rates
+#' @name name the name of the dataset (initial values)
+#' @name cache the cache environment
+#' @return ggplot object
+plot_gam_fit_input_rates <- function(name="initial_values_data",cache) {
+  plot<-cache[["initial_values_data"]]$gam_fit_input %>%
+    dplyr::group_by(sx, sy) %>%
+    dplyr::summarize(y = mean(y), pop = mean(pop)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(y = ifelse(y == 0, 1e-99, y)) %>%
+    ggplot() +
+    geom_tile(aes(x = sx, y = sy, fill = y / pop)) +
+    taxdat::map_theme() +
+    taxdat::color_scale(type = "rates", use_case = "ggplot map", use_log = TRUE)+
+    ggplot2::labs(fill="Incidence rate")
+  return(plot)
+}
+
+#' @name plot_gam_fit_output_cases
+#' @title plot_gam_fit_output_cases
+#' @description plot the rasters with gam fitted output cases
+#' @name name the name of the dataset (initial values)
+#' @name cache the cache environment
+#' @return ggplot object
+plot_gam_fit_output_cases <- function(name="gam_output_df",cache) {
+  plot<-cache[[name]] %>%
+    ggplot() +
+    geom_tile(aes(x = sx, y = sy, fill =y)) +
+    taxdat::map_theme() +
+    taxdat::color_scale(type = "cases", use_case = "ggplot map", use_log = FALSE)+
+    ggplot2::labs(fill="Cases")
+  return(plot)
+}
+
+
+#' @name plot_gam_fit_output_rates
+#' @title plot_gam_fit_input_rates
+#' @description plot the rasters with gam fitted output rates
+#' @name name the name of the dataset (initial values)
+#' @name cache the cache environment
+#' @return ggplot object
+plot_gam_fit_output_rates <- function(name="gam_output_df",cache) {
+  plot<-  cache[[name]] %>%
+    ggplot() +
+    geom_tile(aes(x = sx, y = sy, fill =lambda)) +
+    taxdat::map_theme() +
+    taxdat::color_scale(type = "rates", use_case = "ggplot map", use_log = TRUE)+
+    ggplot2::labs(fill="Incidence rate")
+  return(plot)
+}
