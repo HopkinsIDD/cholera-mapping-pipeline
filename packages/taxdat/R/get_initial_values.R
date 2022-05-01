c40d67731e9e29e169aa3f75c971481d40020ef1
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-05-01 17:57:23 -0400

    Update get_initial_values.R

--- packages/taxdat/R/get_initial_values.R
+++ packages/taxdat/R/get_initial_values.R
@@ -6,7 +6,7 @@
 #' @param cache 
 #' @return initicial values
 get_initial_values_no_cache <- function(config, cache, cholera_directory) {
-  config <- yaml::read_yaml(paste0(params$cholera_directory, params$config))
+  config <- yaml::read_yaml(paste0(cholera_directory, config))
   file_names <- taxdat::get_filenames(config, cholera_directory)
   initial_values_data <- read_file_of_type(file_names["initial_values"], "initial_values_data")
   require(bit64)
