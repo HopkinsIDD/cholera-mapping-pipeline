f4bf049e7b2af39de694ea2fdbd4d01c124b9c1f
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-05-03 15:18:31 -0400

    Update plot_WHOcomparison_table.R

--- packages/taxdat/R/plot_WHOcomparison_table.R
+++ packages/taxdat/R/plot_WHOcomparison_table.R
@@ -24,13 +24,11 @@ plot_WHOcomparison_table <- function(config, cache, cholera_directory) {
                                                               database_api_key_rfile = stringr::str_c(cholera_directory, "/Analysis/R/database_api_key.R"))
  
   if(!is.null(who_annual_cases_from_db)) {
-    who_comparison_table <- who_annual_cases_from_db %>%
+    who_annual_cases_from_db %>%
       as.data.frame() %>%
       dplyr::select(OC_UID, TL, TR, observed, modeled) %>%
-      dplyr::mutate_if(is.numeric, function(x) {format(round(x) , big.mark=",")}) #%>%
-      #dplyr::rename(col.names = c("OC id", "start time", "end time", "# Observed cases", "# Modeled Cases")) #%>%
-      #dplyr::rename( `OC id`=OC_UID, `start time`=TL, `end time`=TR, `Observed cases`=observed, `Modeled Cases`=modeled)
+      dplyr::mutate_if(is.numeric, function(x) {format(round(x) , big.mark=",")}) %>%
+      kableExtra::kable(col.names = c("OC id", "start time", "end time", "# Observed cases", "# Modeled Cases")) %>%
+      kableExtra::kable_styling(bootstrap_options = c("striped"))
   }
-  
-  return(who_comparison_table)
 }
