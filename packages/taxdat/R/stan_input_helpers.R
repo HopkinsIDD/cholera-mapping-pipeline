937b905d5696971363c4c6fb1aafb700ff898b5d
javierps <javier.perezsaez@gmail.com>
2022-05-11 16:21:40 +0200

    fix error in pop_weight extraction

--- packages/taxdat/R/stan_input_helpers.R
+++ packages/taxdat/R/stan_input_helpers.R
@@ -320,7 +320,10 @@ get_space_time_ind_speedup <- function(df,
   }
   
   # Get set of population 1km weights
-  u_loc_grid_weights <- purrr::map_dbl(map_loc_grid_grid ~ lp_dict$pop_weight[lp_dict$upd_long_id == .])
+  u_loc_grid_weights <- purrr::map_dbl(
+    1:length(map_loc_grid_loc),
+    ~ with(lp_dict, pop_weight[upd_long_id == map_loc_grid_grid[.] & 
+                           loctime_id == u_loctimes[map_loc_grid_loc[.]]]))
   
   # Adjust location period mapping ids to reindexed ids
   map_obs_loctime_loc <- u_loctimes_ind[as.character(map_obs_loctime_loc)]
