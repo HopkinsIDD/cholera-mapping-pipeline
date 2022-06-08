7301247f84fd0ba7aed521530680362accb11f68
jkamins7 <jkaminsky@jhu.edu>
2022-06-08 08:03:39 -0400

    Added aoi cropping to prepare_grid

--- Analysis/R/prepare_grid.R
+++ Analysis/R/prepare_grid.R
@@ -12,39 +12,55 @@ prepare_grid <- function(
     dbuser,
     cholera_directory,
     res_space,
-    ingest = T
+    ingest = T,
+    aoi_name = 'raw'
 ) {
-  
+
   # Preamble ---------------------------------------------------------------------
-  
-  
+
+
   # Inputs ------------------------------------------------------------------
-  
+
   # User-defined parameters
   # Spatial resolution
   res_x <- res_space    # longitude resolution in km
   res_y <- res_space    # latitude resolution in km
-  
+
   # Other parameters
   km_to_deg <- 1 / 110.57    # how many degrees is a km at the equator
-  
+
   # Other objects
   conn_pg <- taxdat::connect_to_db(dbuser)
-  
+
+  # Area of interest (for cropping rasters pre-ingestion)
+  # This is mostly for testing purposes
+  aois <- list(
+    list(name = "raw", extent = NULL),
+    list(name = "SSD", extent = raster::extent(23, 37, 3, 13)),   # SSD
+    list(name = "KEN", extent = raster::extent(33, 42, -5.2, 5)),   # Kenya
+    list(name = "SSA", extent = raster::extent(-18.8, 52.6, -35.4, 28))   # SSA
+  )
+  aoi_names <- purrr::map_chr(aois, "name")
+  if (!(aoi_name %in% aoi_names)) {
+    stop("Area of interest ", aoi_name, " not among pre-defined areas (", stringr::str_c(aoi_names, collapse = ","), ")")
+  } else {
+    aoi <- aois[[which(aoi_name == aoi_names)]]
+  }
+
   # Process grid ------------------------------------------------------------------
-  
+
   cat("**** PROCESSING GRID ****\n")
-  
+
   # Check if the master grid is set
   if (DBI::dbExistsTable(conn = conn_pg, DBI::Id(schema = "grids", table = "master_grid"))) {
-    
+
     cat("---- Found master grid \n")
-    
+
   } else {
-    
+
     if (!ingest)
       stop("Couldn't find master grid in database. This needs to be ingested by authorized users.")
-    
+
     # make sure this exists
     master_grid_url <- paste0(
       "ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/",
@@ -57,11 +73,11 @@ prepare_grid <- function(
     if (!dir.exists("Layers/pop_old")) {
       dir.create("Layers/pop_old")
     }
-    
+
     master_grid_filename <- paste0("Layers/pop_old/ppp_", 2020, "_1km_Aggregated.tif")
-   
+
     cat("---- Couldn't find master grid, importing it from", master_grid_filename, "\n")
-    
+
     if (!all(file.exists(master_grid_filename))) {
       missing_indices <- which(!file.exists(master_grid_filename))
       for (missing_index in missing_indices) {
@@ -71,7 +87,15 @@ prepare_grid <- function(
     if (!all(file.exists(master_grid_filename))) {
       stop("The file doesn't exists, try pulling it with git-lfs")
     }
-    
+
+    ## Adding cropping to aoi:
+    r <- raster::stack(master_grid_filename)
+    if (!is.null(aoi$extent)) {
+      cat(paste("Cropping",master_grid_filename, "to", "[", stringr::str_c(c("xmin:", ", xmax:", ", ymin:", ", ymax:"), as.vector(aoi_extent)), "]\n"))
+      r <- raster::crop(master_grid_filename, aoi$extent)
+    }
+    master_grid_filename <- gsub('.tif$', paste0('.cropped.',aoi_name, '.tif'))
+    raster::writeRaster(x = r, filename = master_grid_filename)
 
     r2psql_cmd <- glue::glue("raster2pgsql -s EPSGS:4326 -I -t auto -d {master_grid_filename}  grids.master_grid |
                      psql -d cholera_covariates")
@@ -79,32 +103,32 @@ prepare_grid <- function(
     if (err != 0) {
       stop(paste("System command", r2psql_cmd, "failed"))
     }
-    
+
     update_query <- "UPDATE grids.master_grid SET rast = ST_Reclass(rast, 1, '[0-1000000]:1', '32BF', 0);"
     DBI::dbClearResult(DBI::dbSendStatement(conn_pg, update_query))
     DBI::dbClearResult(DBI::dbSendStatement(conn_pg, "UPDATE grids.master_grid SET rast = ST_SetBandNoDataValue(rast,1, NULL);"))
     DBI::dbClearResult(DBI::dbSendStatement(conn_pg, "SELECT AddRasterConstraints('grids'::name, 'master_grid'::name, 'rast'::name);"))
   }
-  
+
   # Set the desired reference grid name
   grid_name <- stringr::str_c("grid_", res_x, "_", res_y)
   grid_in_db <- taxdat::db_exists_table_multi(conn_pg, c("public", "grids"), grid_name)
-  
+
   if (sum(grid_in_db) == 0) {
-    
+
     if (!ingest)
       stop(glue::glue(paste(
         "Couldn't find reference grid at spatial resolution {res_x}x{res_y}km in database.",
         "This needs to be ingested by authorized users."
       )))
-    
+
     cat("Couldn't find grid at", res_x, "x", res_y, "[km] resolution, computing it.\n")
-    
+
     tmp_rast <- stringr::str_c(raster::tmpDir(), "grid_resampled.tif")
     ref_grid <- glue::glue(
       "PG:\"dbname=cholera_covariates schema=grids table=master_grid user={dbuser} mode=2\""
     )
-    
+
     # Aggregate
     taxdat::gdalwarp2(ref_grid, tmp_rast,
                       tr = c(res_x, res_y) * km_to_deg,
@@ -118,23 +142,23 @@ prepare_grid <- function(
     if (err != 0) {
       stop(paste("System command", r2psql_cmd, "failed"))
     }
-    
+
     # Create centroids table and spatial index
     taxdat::build_geoms_query(conn_pg, schema = "public", grid_name, "centroids")
     taxdat::build_geoms_query(conn_pg, schema = "public", grid_name, "polygons")
-    
+
     # Set the schema to public
     grid_schema <- "public"
-    
+
   } else {
     grid_schema <- names(grid_in_db)[grid_in_db][1]
-    
+
     cat("---- Found grid at ", res_x, "x", res_y, " km resolution in schema '", grid_schema, "'\n", sep = "")
   }
-  
+
   full_grid_name <- stringr::str_c(grid_schema, grid_name, sep = ".")
-  
+
   cat("**** DONE GRID ****\n")
-  
+
   return(full_grid_name)
 }
