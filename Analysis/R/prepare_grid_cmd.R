27e36ec195110b05ca2d0d1562cde2f4ee34b646
jkamins7 <jkaminsky@jhu.edu>
2022-06-08 10:28:24 -0400

    Fixed typo

--- Analysis/R/prepare_grid_cmd.R
+++ Analysis/R/prepare_grid_cmd.R
@@ -2,7 +2,7 @@ option_list <- list(
   optparse::make_option(c("-u", "--dbuser"), action = "store", default =  Sys.getenv("USER"), type="character", help = "Database user"),
   optparse::make_option(c("-d", "--cholera_directory"), action = "store", default = "../", type="character", help = "Cholera directory"),
   optparse::make_option(c("-r", "--res_space"), action = "store", default = 20, type="numeric", help = "Temporal aggregations"),
-  optparse::make_option(c("-i", "--ingest"), action = "store", default = T, type="logical", help = "Flag to do ingestion, if false stops if grid has not already been preprocessed")
+  optparse::make_option(c("-i", "--ingest"), action = "store", default = T, type="logical", help = "Flag to do ingestion, if false stops if grid has not already been preprocessed"),
   optparse::make_option(c("-a", "--area_of_interest"), action = "store", default = 'raw', type="character", help = "Name of extent of grid"),
 )
 
