796f97be4f3c5749ae31821e1fb4d23275caa084
jkamins7 <jkaminsky@jhu.edu>
2022-06-08 09:06:58 -0400

    Actually passing arugment

--- Analysis/R/prepare_grid_cmd.R
+++ Analysis/R/prepare_grid_cmd.R
@@ -3,6 +3,7 @@ option_list <- list(
   optparse::make_option(c("-d", "--cholera_directory"), action = "store", default = "../", type="character", help = "Cholera directory"),
   optparse::make_option(c("-r", "--res_space"), action = "store", default = 20, type="numeric", help = "Temporal aggregations"),
   optparse::make_option(c("-i", "--ingest"), action = "store", default = T, type="logical", help = "Flag to do ingestion, if false stops if grid has not already been preprocessed")
+  optparse::make_option(c("-a", "--area_of_interest"), action = "store", default = 'raw', type="character", help = "Name of extent of grid"),
 )
 
 parser <- optparse::OptionParser(option_list=option_list)
@@ -14,5 +15,6 @@ prepare_grid(
   dbuser = opt$dbuser,
   cholera_directory = opt$cholera_directory,
   res_space = opt$res_space,
-  ingest = opt$ingest
+  ingest = opt$ingest,
+  aoi = opt$area_of_interest
 )
