0ff88749c34498a4cbee93d1b8d5a61b118b4fe4
jkamins7 <jkaminsky@jhu.edu>
2022-06-17 15:27:53 -0400

    Fixed a bug with tfrac allowed to be 0

--- packages/taxdat/R/testing_database.R
+++ packages/taxdat/R/testing_database.R
@@ -798,7 +798,7 @@ SELECT
   observation_data.observation_id,
   location_periods.id as location_period_id,
   temporal_grid.id as temporal_grid_id,
-  (least(observation_data.time_right, temporal_grid.time_max) - greatest(observation_data.time_left, temporal_grid.time_min)) * 1.::double precision / (temporal_grid.time_max - temporal_grid.time_min) as tfrac
+  ( 1 + least(observation_data.time_right, temporal_grid.time_max) - greatest(observation_data.time_left, temporal_grid.time_min)) * 1.::double precision / ( 1 + temporal_grid.time_max - temporal_grid.time_min) as tfrac
 FROM
   observation_data
 INNER JOIN
