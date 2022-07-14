4b24e4f8d504376eb8f217d067bb43c76e4b9908
jkamins7 <jkaminsky@jhu.edu>
2022-07-14 11:01:23 -0400

    Passing that test case broke something important. Probably there's a bug in the test case

--- packages/taxdat/R/testing_database.R
+++ packages/taxdat/R/testing_database.R
@@ -148,20 +148,30 @@ CREATE MATERIALIZED VIEW grids.resized_spatial_grids AS
   add_and_or_drop(psql_connection, add_query, drop_query, drop)
 }
 
+#' @description Create a covariates schema for use in testing
+#' @name create_covariates_schema
+#' @title create_covariates_schema
+#' @param psql_connection a connection to a database made with dbConnect
+#' @param drop Whether to drop an existing table
+create_covariates_schema <- function(psql_connection, drop = FALSE) {
+  add_query <- "CREATE SCHEMA IF NOT EXISTS covariates;"
+  drop_query <- "DROP SCHEMA IF EXISTS covariates CASCADE;"
+  add_and_or_drop(psql_connection, add_query, drop_query, drop)
+}
 #' @description Create a all_covariates table for use in testing
 #' @name create_all_covariates_table
 #' @title create_all_covariates_table
 #' @param psql_connection a connection to a database made with dbConnect
 #' @param drop Whether to drop an existing table
 create_all_covariates_table <- function(psql_connection, drop = FALSE) {
-  schema_query <- "CREATE SCHEMA IF NOT EXISTS covariates;"
-  drop_query <- "DROP SCHEMA IF EXISTS covariates CASCADE;"
+  create_covariates_schema(psql_connection, drop = drop)
+  drop_query <- "DROP TABLE IF EXISTS covariates.all_covariates"
   add_query <- c(
     "CREATE TABLE covariates.all_covariates(covariate_name text, time_left date, time_right date, rid integer, rast raster);",
     "CREATE INDEX covariate_name_size_idx ON covariates.all_covariates(covariate_name, time_left, time_right, rid);"
   )
 
-  add_and_or_drop(psql_connection, c(schema_query, add_query), drop_query, drop)
+  add_and_or_drop(psql_connection, add_query, drop_query, drop)
 }
 
 #' @description Create a time_bounds table for use in testing
@@ -1390,7 +1400,7 @@ convert_simulated_covariates_to_test_covariate_funs <- function(original_simulat
     max_time_index <- max(covariate$t)
     lapply(unique(covariate$t), function(time_index) {
       return(list(
-        name = ifelse(covariate_idx == 0, "population", paste("covariate",
+        name = ifelse(covariate_idx == 1, "population", paste("covariate",
           covariate_idx,
           sep = ""
         )),
