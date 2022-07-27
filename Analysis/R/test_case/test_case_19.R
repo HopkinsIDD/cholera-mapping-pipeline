5f16ba550ae0ac8e811af871a99b2671d0bacb52
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-07-27 11:33:07 -0400

    Update test_case_19.R

--- Analysis/R/test_case/test_case_19.R
+++ Analysis/R/test_case/test_case_19.R
@@ -352,13 +352,9 @@ all_dfs$observations_df <- test_observations %>%
   )
 
 # overlapping observations with consistent case counts
-all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$suspected_cases <- sum(all_dfs$observations_df[grep(
-  "1::",
-  all_dfs$observations_df$qualified_name
-), ]$suspected_cases)
+all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$cases <- all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$cases
 
 test_true_grid_cases<-test_underlying_distribution$mean
-
 #label grids that is observed
 observed_polygon_id<-c(unique(data.frame(sf::st_join(st_centroid(test_true_grid_cases),sf::st_as_sf(all_dfs$observations_df)))%>%subset(is.na(location)==F)%>%subset(!qualified_name=="1")%>%dplyr::select(id)))
 observed_test_true_grid_cases<-test_true_grid_cases%>%subset(id%in%observed_polygon_id$id)
@@ -407,8 +403,7 @@ config <- list(general = list(
   polygonal = covariates_table$polygonal, radiating = covariates_table$radiating,
   constant = covariates_table$constant, Data_simulation_covariates = covariates_table$Data_simulation_covariates,
   Model_covariates = covariates_table$Model_covariates, Observations_with_inconsistent_data = paste0(
-    "Nationally reported data is ",
-    all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$suspected_cases / sum(all_dfs$observations_df[grep("1::", all_dfs$observations_df$qualified_name), ]$suspected_cases), " times of the cases reported at the subnational level."
+    "Nationally reported data is 1 times of the cases reported at the subnational level."
   ),
   Loc_with_inconsistent_data = "-", Cov_data_simulation_filename = "/home/app/cmp/Analysis/output/test_case_19_data_simulation_covariates.rdata",test_true_grid_case_filename="/home/app/cmp/Analysis/output/test_case_19_true_grid_cases.rdata"
   
