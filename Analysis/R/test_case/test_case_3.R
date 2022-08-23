adce0033350a4c96b0810790bb7dcc1330915961
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-08-23 15:04:18 -0400

    Update test_case_3.R

--- Analysis/R/test_case/test_case_3.R
+++ Analysis/R/test_case/test_case_3.R
@@ -355,7 +355,7 @@ all_dfs$observations_df <- test_observations %>%
   )
 
 # overlapping observations with consistent case counts
-all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$cases  <- all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$cases * 3
+all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$suspected_cases  <- all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$suspected_cases * 3
 
 test_true_grid_cases <- test_underlying_distribution$mean
 
@@ -396,7 +396,7 @@ config <- list(general = list(
   directory = rprojroot::find_root_file(
     criterion = ".choldir",
     "Analysis", "Stan"
-  ), ncores = 4, model = "dagar_seasonal_flexible.stan", niter = as.integer(Sys.getenv("CHOLERA_TEST_ITERATION", 4000)),
+  ), ncores = 4, model = "dagar_seasonal_flexible.stan", niter = as.integer(Sys.getenv("CHOLERA_TEST_ITERATION", 100)),
   recompile = TRUE
 ), file_names = list(stan_input = rprojroot::find_root_file(
   criterion = ".choldir",
@@ -415,7 +415,7 @@ config <- list(general = list(
   constant = covariates_table$constant, Data_simulation_covariates = covariates_table$Data_simulation_covariates,
   Model_covariates = covariates_table$Model_covariates, Observations_with_inconsistent_data = paste0(
     "Nationally reported data is ",
-    all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$suspected_cases / sum(all_dfs$observations_df[grep("1::", all_dfs$observations_df$qualified_name), ]$suspected_cases), " times of the cases reported at the subnational level."
+    all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$cases / sum(all_dfs$observations_df[grep("1::", all_dfs$observations_df$qualified_name), ]$cases), " times of the cases reported at the subnational level."
   ),
   Loc_with_inconsistent_data = "-",
   Cov_data_simulation_filename = file.path(rprojroot::find_root(criterion = ".choldir"), "Analysis", "data", "test_case_3_data_simulation_covariates.rds"),
