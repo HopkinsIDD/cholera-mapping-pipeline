466dbccd89047bd48aad2497ecb686f7db4afce5
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-09-19 13:01:45 -0400

    Update transform_covariates.R

--- packages/taxdat/R/transform_covariates.R
+++ packages/taxdat/R/transform_covariates.R
@@ -4,14 +4,22 @@
 #' @param verbose boolean whether to print debug output
 #' @export
 transform_covariates <- function(covariates, transformations, verbose = FALSE) {
+  new_cov_array<-array(dim=c(dim(covariates)[1],dim(covariates)[2],dim(covariates)[3]+length(transformations)))
+  new_cov_array[,,1:dim(covariates)[3]]<-covariates
+  dimnames(new_cov_array)[[3]]<-c(dimnames(covariates)[[3]],rep(NA,length(transformations)))
+  
+  new_id<-1
   for (transformation in transformations) {
-    if (!(transformation[["name"]] %in% names(covariates[1,1,]))) {#QZ: update to obtain the name of the covariates
+    if (!(transformation[["name"]] %in% names(covariates[1,1,]))) {#QZ: update to obtain the name of the covariate
       stop(paste("Trying to perform a transform on a covariate (", transformation[["name"]], ") that does not exist. Allowed covariates are", paste(names(covariates), collapse = ", ")))
     }
     if (verbose) {
       print(paste("Transforming", transformation[["name"]], "by", transformation[["transform_name"]]))
     }
-    covariates[, , transformation[["name"]]] <- transformation[["transform_function"]](covariates[ , ,transformation[["name"]]])
+    
+    new_cov_array[, , dim(covariates)[3]+new_id] <- transformation[["transform_function"]](covariates[ , ,transformation[["name"]]])##QZ: add a new covariate
+    dimnames(new_cov_array)[[3]][dim(covariates)[3]+new_id]<-c(transformation[["transform_name"]])
+    new_id<-new_id+1
   }
-  return(covariates)
+  return(new_cov_array)
 }
