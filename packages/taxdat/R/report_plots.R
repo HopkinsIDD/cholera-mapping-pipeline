106ae775561cecd1ea1bb614615167382c620ae7
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-09-26 14:12:26 -0400

    Update report_plots.R

--- packages/taxdat/R/report_plots.R
+++ packages/taxdat/R/report_plots.R
@@ -503,6 +503,34 @@ plot_rhat <- function(config, cache, cholera_directory) {
   return(plt)
 }
 
+#' @export
+plot_error_by_grid <- function(config, cache, cholera_directory) {
+  get_error_by_grid(cache=cache,cholera_directory = cholera_directory,config=config)
+  cache[["error_by_grid_list"]]%>%
+    kableExtra::kable() %>%
+    kableExtra::kable_styling(bootstrap_options = c("striped"))
+}
+
+#' @export
+plot_error_by_grid_spatial_scale <- function(cache,config,cholera_directory){
+  get_error_by_grid_spatial_scale(cache=cache,cholera_directory = cholera_directory,config=config)
+  cache[["error_by_grid_spatial_scale"]]%>%
+    kableExtra::kable() %>%
+    kableExtra::kable_styling(bootstrap_options = c("striped"))
+}
+
+#' @export
+plot_w_mean<-function(cache,config,cholera_directory){
+  get_cmdstan_fit(config = params$config, cache = cache, cholera_directory = params$cholera_directory)
+  w_data = cache[['cmdstan_fit']]$draws(variables = 'w') %>%
+    reshape2::melt()
+  w_mean = w_data %>% group_by(variable) %>% summarise(mean = mean(value))#QZ: mean across chain and iterations
+  get_covar_cube(cache=cache,config=params$config,cholera_directory=params$cholera_directory)
+  plot_w_mean_data<-cache[["covar_cube"]]%>%subset(t==1)
+  plot_w_mean_data$w_mean=w_mean$mean
+  return(plot(plot_w_mean_data[,c("w_mean",'geometry')]))
+}
+
 # Leaving this here commented out for now
 ### plot_energy <- function(stan_model, par = "all") {
 ###   energy <- sapply(rstan::get_sampler_params(stan_model), function(x) {
