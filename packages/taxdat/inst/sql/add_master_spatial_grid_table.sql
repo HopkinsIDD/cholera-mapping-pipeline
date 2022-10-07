be35eb0d3e9ddf56b88618f53f07348ec928e9bf
Joshua Kaminsky <jkaminsky@jhu.edu>
2022-10-07 09:26:03 -0400

    Fixed strangeness with resized grids

--- packages/taxdat/inst/sql/add_master_spatial_grid_table.sql
+++ packages/taxdat/inst/sql/add_master_spatial_grid_table.sql
@@ -3,7 +3,7 @@ SELECT rid,rast FROM (
   SELECT
     rank() OVER (ORDER BY all_covariates.filename) AS rnk,
     rid,
-    rast
+    st_addband(st_makeemptyraster(all_covariates.rast), '1BB'::text, 0, NULL) as rast
   FROM
     covariates.all_covariates
   INNER JOIN
