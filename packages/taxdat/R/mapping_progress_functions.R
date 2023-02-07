9fd89df3b1bdd1c1713d53822cd136c668264d90
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:16:38 -0500

    Update mapping_progress_functions.R

--- packages/taxdat/R/mapping_progress_functions.R
+++ packages/taxdat/R/mapping_progress_functions.R
@@ -1,3 +1,5 @@
+#' @include file_name_functions.R
+
 #' @title create_mapping_progress_table
 #' @name create_mapping_progress_table
 #' @description From a list of configs (or directories containing only configs), create a table displaying the files which exist for those configs, detailing which are present and which are absent
@@ -5,6 +7,7 @@
 #' @return a data.frame with rows based on configs, and columns based on different stages of progress, with TRUE if the stage has been completed for that config, and FALSE if it has not
 #' @export
 create_mapping_progress_table <- function(config_path, pipeline_path) {
+
   ## Turn directories into more files
   if (any(dir.exists(config_path))) {
     dir_indices <- dir.exists(config_path)
@@ -14,8 +17,7 @@ create_mapping_progress_table <- function(config_path, pipeline_path) {
   }
 
   ## bind together the results for each config
-  do.call(
-    what = rbind,
+  do.call(what=rbind,
     lapply(
       config_path,
       create_mapping_progress_table_single_config,
@@ -40,3 +42,27 @@ create_mapping_progress_table_single_config <- function(config_path, pipeline_pa
     dplyr::mutate(config_path = config_path) %>%
     return()
 }
+
+### create_mapping_progress_table(
+###   c("Analysis/configs/2010_2016_full_base", "Analysis/configs/2015_2019_full_base"),
+###   "~/git/cholera-mapping-pipeline"
+### ) %>%
+###   group_by(config_path) %>%
+###   summarize(stages_complete = ifelse(
+###     data,
+###     ifelse(covar,
+###       ifelse(stan_input,
+###         ifelse(stan_output,
+###           "Output Produced",
+###           "Input Produced"
+###         ), "Covariates constructed"
+###       ),
+###       "Data pulled"
+###     ),
+###     "None"
+###   )) %>%
+###   tidyr::separate(config_path,
+###     c(NA, NA, "start_year","end_year", NA, NA, "country", NA) # This is file path specific
+###   ) %>%
+###   ggplot() +
+###   geom_tile(aes(x=paste(start_year, end_year, sep = '-'), y=country, fill=stages_complete))
