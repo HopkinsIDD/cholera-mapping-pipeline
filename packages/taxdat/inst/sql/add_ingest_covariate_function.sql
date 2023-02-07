4bbc7c8b8712c82ca67d4da41984ba4567230f0e
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:18:09 -0500

    Update add_ingest_covariate_function.sql

--- packages/taxdat/inst/sql/add_ingest_covariate_function.sql
+++ packages/taxdat/inst/sql/add_ingest_covariate_function.sql
@@ -1,11 +1,11 @@
-CREATE FUNCTION ingest_covariate(collection_id bigint, table_name text, ingest_time_left date, ingest_time_right date)
+create or replace function ingest_covariate(name text, table_name text, ingest_time_left date, ingest_time_right date)
   returns void
   LANGUAGE plpgsql
   as $$
   begin
-  PERFORM * FROM covariates.all_covariates where all_covariates.raster_covariate_collection_id = collection_id AND all_covariates.filename = table_name AND all_covariates.time_left = ingest_time_left and all_covariates.time_right = ingest_time_right;
+  PERFORM * FROM covariates.all_covariates where all_covariates.covariate_name = name AND all_covariates.time_left = ingest_time_left and all_covariates.time_right = ingest_time_right;
   if not found then
-  EXECUTE FORMAT ('INSERT INTO covariates.all_covariates SELECT %L as raster_covariate_collection_id, %L as filename, %L as time_left, %L as time_right, rid, rast FROM covariates.%I', collection_id, table_name, ingest_time_left, ingest_time_right, table_name);
+  EXECUTE FORMAT ('INSERT INTO covariates.all_covariates SELECT %L as name, %L as time_left, %L as time_right, rid, rast FROM covariates.%I', name, ingest_time_left, ingest_time_right, table_name);
   end if;
   end;
   $$ SECURITY DEFINER;
