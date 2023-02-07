558cb91e3597396c6d9201de1253335170ef8def
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:17:26 -0500

    Update add_shape_resized_spatial_grid_map_view.sql

--- packages/taxdat/inst/sql/add_shape_resized_spatial_grid_map_view.sql
+++ packages/taxdat/inst/sql/add_shape_resized_spatial_grid_map_view.sql
@@ -1,12 +1,9 @@
-CREATE MATERIALIZED VIEW shape_resized_spatial_grid_map_view AS (
-  SELECT l.qualified_name,
-         l.location_period_id as location_period_id,
-         l.shape_id, p.id as grid_id,
-         ST_Intersection(p.polygon, l.geom) as intersection_geom,
-         p.polygon as grid_geom
-  FROM
-    shapes_with_names l,
-    grids.resized_spatial_grid_pixels p
-  WHERE
-    l.valid = true AND
-    ST_Intersects(p.polygon, l.boundary) );
+CREATE MATERIALIZED VIEW IF NOT EXISTS shape_resized_spatial_grid_map_view AS(
+SELECT  l.qualified_name, l.location_period_id as location_period_id, l.shape_id, p.id as grid_id, ST_Intersection(p.polygon, l.geom) as intersection_geom, p.polygon as grid_geom
+FROM
+  grids.resized_spatial_grid_pixels p,
+  shapes_with_names l
+WHERE
+ST_IsValid(l.geom)
+AND ST_Intersects(p.polygon, ST_Boundary(l.geom))
+);
