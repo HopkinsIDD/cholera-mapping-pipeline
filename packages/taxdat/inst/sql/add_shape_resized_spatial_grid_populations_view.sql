9e6d3b6ea87198f9fd9c05db0d78a9b834217231
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:17:23 -0500

    Update add_shape_resized_spatial_grid_populations_view.sql

--- packages/taxdat/inst/sql/add_shape_resized_spatial_grid_populations_view.sql
+++ packages/taxdat/inst/sql/add_shape_resized_spatial_grid_populations_view.sql
@@ -1,10 +1,10 @@
-CREATE MATERIALIZED VIEW shape_resized_spatial_grid_populations as (select
+CREATE MATERIALIZED VIEW IF NOT EXISTS shape_resized_spatial_grid_populations as (select
   shape_resized_spatial_grid_map_view.location_period_id,
   shape_resized_spatial_grid_map_view.shape_id,
   shape_resized_spatial_grid_map_view.grid_id,
   all_covariates.time_left,
   all_covariates.time_right,
-  all_covariates.filename,
+  all_covariates.covariate_name,
   (st_summarystats(st_clip(rast,intersection_geom))).sum as intersection_population,
   (st_summarystats(st_clip(rast,grid_geom))).sum as grid_population
 FROM
@@ -13,6 +13,5 @@ INNER JOIN
   covariates.all_covariates
     on
       st_intersects(shape_resized_spatial_grid_map_view.intersection_geom, st_envelope(all_covariates.rast))
-INNER JOIN public.raster_covariate_collections ON all_covariates.raster_covariate_collection_id = raster_covariate_collections.id
 WHERE
-  raster_covariate_collections.name = 'population');
+  covariate_name = 'population');
