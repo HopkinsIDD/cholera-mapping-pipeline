33837e2022443d3a800c93225f42fd2136c67382
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:17:50 -0500

    Update add_pull_minimal_grid_population_function.sql

--- packages/taxdat/inst/sql/add_pull_minimal_grid_population_function.sql
+++ packages/taxdat/inst/sql/add_pull_minimal_grid_population_function.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION pull_minimal_grid_population(location_name text, start_date date, end_date date, time_scale text)
+create or replace function pull_minimal_grid_population(location_name text, start_date date, end_date date, time_scale text)
   returns table(
     rid int,
     temporal_grid_id bigint,
@@ -27,13 +27,9 @@ INNER JOIN
     ON
       covariates.all_covariates.time_left <= temporal_grid.time_midpoint
       AND covariates.all_covariates.time_right >= temporal_grid.time_midpoint
-INNER
-  JOIN public.raster_covariate_collections
-    ON
-      all_covariates.raster_covariate_collection_id = raster_covariate_collections.id
 WHERE
   time_midpoint >=start_date
   AND time_midpoint <= end_date
-  AND raster_covariate_collections.name = 'population'
+  AND covariate_name = 'population'
   AND locations.qualified_name = location_name
 $$ LANGUAGE SQL SECURITY DEFINER;
