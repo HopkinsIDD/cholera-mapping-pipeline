ef9171d6ad9d143f4fb01768ff0345f7e90be951
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:17:21 -0500

    Update add_shapes_table.sql

--- packages/taxdat/inst/sql/add_shapes_table.sql
+++ packages/taxdat/inst/sql/add_shapes_table.sql
@@ -2,7 +2,5 @@ CREATE TABLE IF NOT EXISTS shapes(
   id BIGSERIAL PRIMARY KEY,
   location_period_id bigint REFERENCES location_periods(id) UNIQUE,
   shape GEOMETRY,
-  box GEOMETRY,
-  boundary GEOMETRY,
-  valid bool
+  box GEOMETRY
 );
