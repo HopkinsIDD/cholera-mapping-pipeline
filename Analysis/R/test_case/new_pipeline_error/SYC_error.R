bea67c10bff0ab35ed518cf97c7da6907c51886f
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:19:20 -0500

    Update SYC_error.R

--- Analysis/R/test_case/new_pipeline_error/SYC_error.R
+++ Analysis/R/test_case/new_pipeline_error/SYC_error.R
@@ -336,7 +336,7 @@ my_seed <- .GlobalEnv$.Random.seed
 
 test_observations <- observe_polygons(
   test_polygons = dplyr::mutate(all_dfs$shapes_df,
-    location = qualified_name, geometry = geom
+                                location = qualified_name, geometry = geom
   ), test_covariates = raster_df, underlying_distribution = test_underlying_distribution,
   noise = FALSE, number_draws = 1, grid_proportion_observed = 1, polygon_proportion_observed = 1,
   min_time_left = query_time_left, max_time_right = query_time_right, seed = my_seed
@@ -354,16 +354,16 @@ all_dfs$observations_df <- test_observations %>%
 all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$suspected_cases <- sum(all_dfs$observations_df[grep(
   "1::",
   all_dfs$observations_df$qualified_name
-), ]$suspected_cases)
+), ]$suspected_cases) 
 
-test_true_grid_cases <- test_underlying_distribution$mean
+test_true_grid_cases<-test_underlying_distribution$mean
 
-# label grids that is observed
-observed_polygon_id <- c(unique(data.frame(sf::st_join(st_centroid(test_true_grid_cases), sf::st_as_sf(all_dfs$observations_df))) %>% subset(is.na(location) == F) %>% subset(!qualified_name == "1") %>% dplyr::select(id)))
-observed_test_true_grid_cases <- test_true_grid_cases %>% subset(id %in% observed_polygon_id$id)
-test_true_grid_cases <- test_true_grid_cases %>% mutate(observed = ifelse(id %in% observed_polygon_id$id, "Observed grid cells", "Unobserved grid cells"))
+#label grids that is observed
+observed_polygon_id<-c(unique(data.frame(sf::st_join(st_centroid(test_true_grid_cases),sf::st_as_sf(all_dfs$observations_df)))%>%subset(is.na(location)==F)%>%subset(!qualified_name=="1")%>%dplyr::select(id)))
+observed_test_true_grid_cases<-test_true_grid_cases%>%subset(id%in%observed_polygon_id$id)
+test_true_grid_cases<-test_true_grid_cases%>%mutate(observed=ifelse(id%in%observed_polygon_id$id,"Observed grid cells","Unobserved grid cells"))
 
-saveRDS(test_true_grid_cases, "/home/app/cmp/Analysis/output/SYC_error_true_grid_cases.rdata")
+saveRDS(test_true_grid_cases,"/home/app/cmp/Analysis/output/SYC_error_true_grid_cases.rdata")
 
 ## ------------------------------------------------------------------------------------------------------------------------
 ## Create Database
@@ -401,7 +401,7 @@ config <- list(general = list(
   nrows = 5, ncols = 5, data_type = "Grid data", oc_type = "-", polygon_type = "Fake polygon",
   polygon_coverage = "100%", randomize = TRUE, ncovariates = nrow(covariates_table),
   single_year_run = ifelse(lubridate::year(query_time_right) - lubridate::year(query_time_left) ==
-    0, "yes", "no"), nonspatial = covariates_table$nonspatial, nontemporal = covariates_table$nontemporal,
+                             0, "yes", "no"), nonspatial = covariates_table$nonspatial, nontemporal = covariates_table$nontemporal,
   spatially_smooth = covariates_table$spatially_smooth, temporally_smooth = covariates_table$temporally_smooth,
   polygonal = covariates_table$polygonal, radiating = covariates_table$radiating,
   constant = covariates_table$constant, Data_simulation_covariates = covariates_table$Data_simulation_covariates,
@@ -409,7 +409,7 @@ config <- list(general = list(
     "Nationally reported data is ",
     all_dfs$observations_df[which(all_dfs$observations_df$qualified_name == "1"), ]$suspected_cases / sum(all_dfs$observations_df[grep("1::", all_dfs$observations_df$qualified_name), ]$suspected_cases), " times of the cases reported at the subnational level."
   ),
-  Loc_with_inconsistent_data = "-", Cov_data_simulation_filename = "/home/app/cmp/Analysis/output/SYC_error_data_simulation_covariates.rdata", test_true_grid_case_filename = "/home/app/cmp/Analysis/output/SYC_error_true_grid_cases.rdata"
+  Loc_with_inconsistent_data = "-", Cov_data_simulation_filename = "/home/app/cmp/Analysis/output/SYC_error_data_simulation_covariates.rdata",test_true_grid_case_filename="/home/app/cmp/Analysis/output/SYC_error_true_grid_cases.rdata"
 ))
 
 yaml::write_yaml(x = config, file = config_filename)
