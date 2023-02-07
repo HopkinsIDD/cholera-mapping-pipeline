cf29a1ba1ee555214bfa89499152c54541d6ee6d
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-02-07 13:18:22 -0500

    Update read_taxonomy_data_example.R

--- packages/taxdat/demo/read_taxonomy_data_example.R
+++ packages/taxdat/demo/read_taxonomy_data_example.R
@@ -2,12 +2,12 @@
 df <- read_taxonomy_data()
 
 ## You can manually specify the taxonomy directory
-df <- read_taxonomy_data("taxonomy-working/working-entry1")
+df <- read_taxonomy_data('taxonomy-working/working-entry1')
 
 ## You can specify which columns you want
-df <- read_taxonomy_data("taxonomy-verified", c("who_region", "ISO_A1", "sCh", "cCh"))
+df <- read_taxonomy_data('taxonomy-verified', c('who_region','ISO_A1', 'sCh', 'cCh'))
 
 ## You can filter the data by passing strings (as in dplyr)
 ## **Note that these only have access to fields in the description file
 
-df <- read_taxonomy_data("taxonomy-verified", NULL, "who_region %in% c('EMR','AFR')", "sCh > 0")
+df <- read_taxonomy_data('taxonomy-verified', NULL, "who_region %in% c('EMR','AFR')", 'sCh > 0')
