c1365d74022daa6e76d6f5dd8401dbb6d8ba43d2
KaiyueZou <kzou7@login02.rockfish.cluster>
2023-02-13 17:56:17 -0500

    added idm submission Shell scripts

---
+++ submission_Shell_scripts/previous/marcc_submit_BEN.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name submit_multiple_years
+#SBATCH --time=01:00:00
+#SBATCH --mem=20G
+#SBATCH --ntasks=4
+#SBATCH --partition=defq
+#SBATCH --account=aazman1
+
+export GCC_VERSION=9.3.0
+export R_VERSION=4.0.2
+module purge
+
+ml gcc/$GCC_VERSION
+ml openmpi
+ml gdal
+ml r/$R_VERSION
+ml r-magrittr
+ml r-optparse
+ml r-yaml
+ml r-rprojroot
+ml r-purrr
+ml r-jsonlite
+ml r-dplyr
+ml r-tidyverse
+ml r-stringi
+ml r-rstan
+
+export CHOLERA_ON_MARCC=TRUE
+export CHOLERA_PIPELINE_DIRECTORY=/data/aazman1/$USER/cholera-mapping-pipeline/
+export CHOLERA_CONFIG=/data/aazman1/$USER/cholera-configs/
+export CHOLERA_CONFIG_DIRECTORY=/data/aazman1/$USER/cholera-configs/no_covariate_production_2016_2020/2016_2020_country
+export CHOLERA_CONFIG=$CHOLERA_CONFIG_DIRECTORY/config_BEN_2016_2020.yml
+export R_LIBRARY_DIRECTORY=$HOME/rlibs/cmp/$R_VERSION/gcc/$GCC_VERSION/
+export CMDSTAN_LOCATION=/data/aazman1/$USER/cmdstan
+
+cd $CHOLERA_PIPELINE_DIRECTORY
+
+Rscript -e "library(taxdat, lib.loc='$R_LIBRARY_DIRECTORY'); library(cmdstanr, lib.loc='$R_LIBRARY_DIRECTORY'); library(withr, lib.loc='$R_LIBRARY_DIRECTORY'); library(processx, lib.loc='$R_LIBRARY_DIRECTORY'); cmdstanr::set_cmdstan_path('$CMDSTAN_LOCATION'); source('Analysis/R/set_parameters.R')"
+
+echo "DONE"
