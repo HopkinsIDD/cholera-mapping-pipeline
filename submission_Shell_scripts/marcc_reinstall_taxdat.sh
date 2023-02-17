962c4b8a3a320081b50bcf93483c1a96768c4dd8
KaiyueZou <kybryant@umich.edu>
2023-02-17 13:28:28 -0500

    updated Shell scripts to facilitate runs on MARCC

--- submission_Shell_scripts/marcc_reinstall_taxdat.sh
+++ submission_Shell_scripts/marcc_reinstall_taxdat.sh
@@ -6,35 +6,22 @@
 #SBATCH --partition=defq
 #SBATCH --account=aazman1
 
-# Specify toolchain
 export GCC_VERSION=9.3.0
 export R_VERSION=4.0.2
-
-# Set filepaths for:
-## Local R libraries
-export CHOLERA_PIPELINE_DIRECTORY=/data/aazman1/$USER/cholera-mapping-pipeline
-export R_LIBRARY_DIRECTORY=$HOME/rlibs/cmp/$R_VERSION/gcc/$GCC_VERSION/
-## cmdstan repo
-# export CMDSTAN_LOCATION=/data/aazman1/$USER/cmdstan
-
-## clone cmdstan (will not fail if already cloned)
-#git clone https://github.com/stan-dev/cmdstan.git $CMDSTAN_LOCATION --recurse-submodules
-#cd $CMDSTAN_LOCATION
-
-## Update cmdstan
-# git reset --hard $CMDSTAN_LOCATION
-# git clean -fxd $CMDSTAN_LOCATION
-# git pull
-
-
-## Set up modules
-## (we need to do git things since reset removes git)
 module purge
 
 ml gcc/$GCC_VERSION
 ml openmpi
 ml gdal
 ml r/$R_VERSION
+ml udunits
+ml proj
+ml libjpeg
+ml sqlite
+ml geos
+ml libpng
+ml curl
+
 ml r-magrittr
 ml r-optparse
 ml r-yaml
@@ -46,6 +33,9 @@ ml r-tidyverse
 ml r-stringi
 ml r-rstan
 
+export CHOLERA_ON_MARCC=TRUE
+export CHOLERA_PIPELINE_DIRECTORY=/data/aazman1/$USER/cholera-mapping-pipeline/
+export R_LIBRARY_DIRECTORY=$HOME/rlibs/cmp/$R_VERSION/gcc/$GCC_VERSION/
 
 ## Install new packages required by the taxdat package and the taxdat package itself
 cd $CHOLERA_PIPELINE_DIRECTORY
@@ -59,6 +49,7 @@ mkdir -p $R_LIBRARY_DIRECTORY \
                     install.packages('roxygen2', lib='$R_LIBRARY_DIRECTORY') }
                 " \
  && Rscript -e "options(error=quit, status = 1); 
+                library(terra, lib='$R_LIBRARY_DIRECTORY')
                 install.packages('$CHOLERA_PIPELINE_DIRECTORY/packages/taxdat', type='source', repos = NULL, lib='$R_LIBRARY_DIRECTORY')"
  ## We need to install these packages again, since they need newer versions than rockfish has available
 
