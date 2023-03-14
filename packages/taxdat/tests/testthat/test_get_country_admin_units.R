b6d252cde230a05aea012e2ceb3ea1dd66272837
qulu <qulu@idmodeling2.jhsph.edu>
2023-03-14 13:28:20 -0400

    updated the unit test

--- packages/taxdat/tests/testthat/test_get_country_admin_units.R
+++ packages/taxdat/tests/testthat/test_get_country_admin_units.R
@@ -1,6 +1,6 @@
 ## testing for using rgeoboundaries shapefiles for certain country's national shapefiles
 
 test_that("get_country_admin_units works", { 
- testthat::expect_message(get_country_admin_units(iso_code="UGA",admin_level=0))
- testthat::expect_no_message(get_country_admin_units(iso_code="AGO",admin_level=0))
+ expect_message(get_country_admin_units(iso_code="UGA",admin_level=0))
+ expect_no_warning(get_country_admin_units(iso_code="AGO",admin_level=0))
 })
