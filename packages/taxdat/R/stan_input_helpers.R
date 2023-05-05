dc0168c1393593448c7680ad6ea426d5daf6e66a
javierps <javier.perezsaez@gmail.com>
2023-05-05 11:20:26 +0200

    fix bug introduced by parallel aggregation to keep the location_name when computing sf_cases_resized

--- packages/taxdat/R/stan_input_helpers.R
+++ packages/taxdat/R/stan_input_helpers.R
@@ -924,7 +924,7 @@ aggregate_observations <- function(sf_cases_resized,
   if (do_parallel) {
     
     df_split <- sf_cases_resized %>%
-      dplyr::group_by(loctime, OC_UID, locationPeriod_id) %>%
+      dplyr::group_by(loctime, OC_UID, locationPeriod_id, location_name) %>%
       dplyr::group_split() 
     
     nchunk <- n_cpus * 5
@@ -945,7 +945,7 @@ aggregate_observations <- function(sf_cases_resized,
     }
   } else {
     sf_cases_resized <- sf_cases_resized %>%
-      dplyr::group_by(loctime, OC_UID, locationPeriod_id) %>%
+      dplyr::group_by(loctime, OC_UID, locationPeriod_id, location_name) %>%
       dplyr::group_modify(.f = aggregate_single_lp, 
                           verbose = verbose,
                           cases_column = cases_column) %>%
