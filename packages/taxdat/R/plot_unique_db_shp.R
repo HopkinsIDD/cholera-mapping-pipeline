699d01ff244f848b325e32facf7bbe99f2b18e49
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-06-15 15:17:40 -0400

    Update plot_unique_db_shp.R

--- packages/taxdat/R/plot_unique_db_shp.R
+++ packages/taxdat/R/plot_unique_db_shp.R
@@ -7,9 +7,9 @@
 #' @param cache
 plot_unique_db_shps <- function(cache,config,cholera_directory){
   taxdat::get_sf_cases_resized(name="sf_cases_resized",cache=cache,config = config,cholera_directory = cholera_directory)
-  taxdat::get_unique_db_shps(cache=cache,config=config,cholera_directory=cholera_directory)
+  unique_db_shps <- taxdat::get_unique_db_shps(name='unique_db_shps',cache=cache,config=config,cholera_directory=cholera_directory)
   plot<-ggplot2::ggplot()+
-    ggplot2::geom_sf(data=cache[["unique_db_shps"]],
+    ggplot2::geom_sf(data=unique_db_shps,
                      alpha=0)
   return(plot)
 }
