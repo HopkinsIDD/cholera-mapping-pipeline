2b751345f6d5580f42117ad7c40bed7e497371db
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-06-16 09:54:01 -0400

    Update plot_unique_db_shp.R

--- packages/taxdat/R/plot_unique_db_shp.R
+++ packages/taxdat/R/plot_unique_db_shp.R
@@ -7,9 +7,24 @@
 #' @param cache
 plot_unique_db_shps <- function(cache,config,cholera_directory){
   taxdat::get_sf_cases_resized(name="sf_cases_resized",cache=cache,config = config,cholera_directory = cholera_directory)
-  unique_db_shps <- taxdat::get_unique_db_shps(name='unique_db_shps',cache=cache,config=config,cholera_directory=cholera_directory)
+  unique_db_shps <- taxdat::get_unique_db_shps(cache=cache,config=config,cholera_directory=cholera_directory)
   plot<-ggplot2::ggplot()+
     ggplot2::geom_sf(data=unique_db_shps,
                      alpha=0)
+  return(plot)
+}
+
+#' @export
+#' @name plot_unique_db_shps_by_time
+#' @param config
+#' @param cholera_directory
+#' @param cache
+plot_unique_db_shps_by_time <- function(cache,config,cholera_directory){
+  taxdat::get_sf_cases_resized(name="sf_cases_resized",cache=cache,config = config,cholera_directory = cholera_directory)
+  unique_db_shps <- taxdat::get_unique_db_shps(cache=cache,config=config,cholera_directory=cholera_directory)
+  plot<-ggplot2::ggplot()+
+    ggplot2::geom_sf(data=unique_db_shps,
+                     alpha=0) +
+    ggplot2::facet_wrap(~time_slice)
   return(plot)
 }
