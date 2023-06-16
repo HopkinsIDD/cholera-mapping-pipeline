2d88e7e41902d398bf29241f75ba0d4fa7a6e849
fang19911030 <fang291831388@gmail.com>
2023-06-16 13:44:24 -0400

    add new function

--- packages/taxdat/R/get_genquant.R
+++ packages/taxdat/R/get_genquant.R
@@ -22,3 +22,42 @@ get_genquant <- cache_fun_results(
   name = "genquant", fun = get_genquant_no_cache,
   overwrite = F, config = config
 )
+
+#' @export
+#' @name get_genquant_summarized_cases_no_cache
+#' @title get_genquant_summarized_cases_no_cache
+#' @description loaded mondeled mean annual cases from genquant output
+#' @param config config file that contains the parameter information
+#' @param cache the cached environment
+#' @param admin_level  select admin 0,1,2 or all
+#' @return the summarized cases
+get_genquant_summarized_cases_no_cache <- function(config,
+cache,
+admin_level = c(0, 1, 2)) {
+    if (!all(admin_level %in% c(0, 1, 2))) {
+        stop("admin_level must in 0, 1, 2.")
+    }
+  cases <- cache[["genquant"]]$summary(
+    variables = "location_cases_output",
+    c(
+      posterior::default_summary_measures(),
+      posterior::default_convergence_measures()
+    ),
+    .cores = 4
+  ) %>%
+    mutate(
+      id = str_extract(variable, "[0-9]+") %>% as.numeric(),
+      location_period_id = cache[["stan_input"]]$fake_output_obs$locationPeriod_id[id],
+      TL = cache[["stan_input"]]$fake_output_obs$TL[id]
+    )
+    return(cases)
+}
+# cache the results
+#' @export
+#' @name get_genquant_summarized_cases
+get_genquant_summarized_cases <- cache_fun_results(
+    "genquant_summarized_cases",
+    get_genquant_summarized_cases_no_cache,
+    overwrite = FALSE,
+    config = config,
+)
