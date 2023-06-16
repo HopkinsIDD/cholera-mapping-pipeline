995ccda1a42b03be43e552aaaf728a0c6145aa31
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-06-16 09:53:58 -0400

    Update plot_polygon_model_output_input.R

--- packages/taxdat/R/plot_polygon_model_output_input.R
+++ packages/taxdat/R/plot_polygon_model_output_input.R
@@ -57,7 +57,7 @@ plot_area_adjusted_observed_cases<- function(config, cache, cholera_directory) {
         mutate(area_adjusted_suspected_cases=as.numeric(attributes.fields.suspected_cases/sf::st_area(geom)))
     
     plot <- plot_sf_with_fill(cache=cache,name = "area_adjusted_observed_polygon_cases_disjoint_aggregated",
-                              color_scale_type = "cases", fill_column = "area_adjusted_suspected_cases")
+                              color_scale_type = "obs_cases", fill_column = "area_adjusted_suspected_cases")
     return(plot)
 }
 
