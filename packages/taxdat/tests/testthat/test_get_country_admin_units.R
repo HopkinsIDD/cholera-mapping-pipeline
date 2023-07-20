20dba79901960bef8007fca6de409b7a4841b80c
javierps <javier.perezsaez@gmail.com>
2023-07-20 15:10:15 +0200

    update the test for pulling the Zanzibar shapefiles

--- packages/taxdat/tests/testthat/test_get_country_admin_units.R
+++ packages/taxdat/tests/testthat/test_get_country_admin_units.R
@@ -3,5 +3,5 @@
 test_that("get_country_admin_units works", { 
  expect_message(get_country_admin_units(iso_code="UGA",admin_level=0),"Using the rgeoboundaries national shapefile for this country.")
  expect_message(get_country_admin_units(iso_code="AGO",admin_level=0),"Using the gadm national shapefile for this country.")
- expect_message(get_country_admin_units(iso_code="ZNZ",admin_level=0),"Using the aggregated gadm shapefiles for this region")
+ expect_message(get_country_admin_units(iso_code="ZNZ",admin_level=0),"Using the aggregated gadm shapefiles for Zanzibar")
 })
