1dcb0debc3bf149734e68010a2b3a9da0fdc694e
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-08-02 13:42:06 -0400

    Update test_get_loctime_combs.R

--- packages/taxdat/tests/testthat/test_get_loctime_combs.R
+++ packages/taxdat/tests/testthat/test_get_loctime_combs.R
@@ -9,10 +9,10 @@ test_that("get_loctime_combs works for single-year obs and single lp", {
   stan_data$map_obs_loctime_loc <- array(rep(1:10,1))
   names(stan_data$map_obs_loctime_loc) <- 1:10
   
-  loctime_combs <- taxdat::get_loctime_combs(stan_data)
+  loctime_combs <- get_loctime_combs(stan_data)
   
-  testthat::expect_equal(length(stan_data$map_obs_loctime_obs),length(loctime_combs))
-  testthat::expect_equal(length(stan_data$map_obs_loctime_loc),length(loctime_combs))
+  expect_equal(length(stan_data$map_obs_loctime_obs),length(loctime_combs))
+  expect_equal(length(stan_data$map_obs_loctime_loc),length(loctime_combs))
   
 })
 
@@ -26,10 +26,10 @@ test_that("get_loctime_combs works for single-year obs and multiple lps", {
   stan_data$map_obs_loctime_loc <- array(c(rep(1:8,1),rep(10,2)))
   names(stan_data$map_obs_loctime_loc) <- 1:10
   
-  loctime_combs <- taxdat::get_loctime_combs(stan_data)
+  loctime_combs <- get_loctime_combs(stan_data)
   
-  testthat::expect_true(length(stan_data$map_obs_loctime_obs) > length(unique(loctime_combs)))
-  testthat::expect_true(length(stan_data$map_obs_loctime_loc) > length(unique(loctime_combs)))
+  expect_true(length(stan_data$map_obs_loctime_obs) > length(unique(loctime_combs)))
+  expect_true(length(stan_data$map_obs_loctime_loc) > length(unique(loctime_combs)))
   
 })
 
@@ -42,10 +42,10 @@ test_that("get_loctime_combs works for multi-year obs and single lp", {
   stan_data$map_obs_loctime_loc <- array(rep(1:10,1))
   names(stan_data$map_obs_loctime_loc) <- 1:10
   
-  loctime_combs <- taxdat::get_loctime_combs(stan_data)
+  loctime_combs <- get_loctime_combs(stan_data)
   
-  testthat::expect_true(length(unique(stan_data$map_obs_loctime_obs)) < length(unique(loctime_combs)))
-  testthat::expect_equal(length(stan_data$map_obs_loctime_loc), length(unique(loctime_combs)))
+  expect_true(length(unique(stan_data$map_obs_loctime_obs)) < length(unique(loctime_combs)))
+  expect_equal(length(stan_data$map_obs_loctime_loc), length(unique(loctime_combs)))
   
 })
 
@@ -58,9 +58,9 @@ test_that("get_loctime_combs works for multi-year obs and multiple lps", {
   stan_data$map_obs_loctime_loc <- array(c(rep(1:8,1),rep(10,2)))
   names(stan_data$map_obs_loctime_loc) <- 1:10
   
-  loctime_combs <- taxdat::get_loctime_combs(stan_data)
+  loctime_combs <- get_loctime_combs(stan_data)
   
-  testthat::expect_true(length(stan_data$map_obs_loctime_obs) > length(unique(loctime_combs)))
-  testthat::expect_true(length(stan_data$map_obs_loctime_loc) > length(unique(loctime_combs)))
+  expect_true(length(stan_data$map_obs_loctime_obs) > length(unique(loctime_combs)))
+  expect_true(length(stan_data$map_obs_loctime_loc) > length(unique(loctime_combs)))
   
 })
