09d1d9c3f6748649f4f0985b814ca8fdedacdfd4
Qulu <44678888+QLLZ@users.noreply.github.com>
2023-08-23 16:24:58 -0400

    first try to add coverage plot to cdr

---
+++ packages/taxdat/R/plot_posterior_coverage_cdr.R
@@ -0,0 +1,14 @@
+plot_posterior_coverage_cdr <- function(config, cache, cholera_directory){
+  
+  config_list <-  yaml::read_yaml(paste0(cholera_directory, "/", config))
+  
+  gen_obs <- taxdat:::postprocess_gen_obs(config_list = config_list)
+  
+  colors_admin_levels <- function(){c("#CAE0C9", "#99CCFF", "#FF9999", "#CC9900", "#FF9933",'black')}
+  
+  gen_obs$country <- config_list$countries_name
+  
+  plot <- taxdat::plot_posterior_coverage(gen_obs)
+  
+  return(plot)
+}
