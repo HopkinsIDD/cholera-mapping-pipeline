5f31698636bcc62c9880704f1625185157d16021
javierps <javier.perezsaez@gmail.com>
2023-09-20 16:12:59 +0200

    Change "Middel Africa" to "Central Africa"

--- packages/taxdat/R/postprocessing.R
+++ packages/taxdat/R/postprocessing.R
@@ -831,7 +831,7 @@ get_AFRO_region <- function(data, ctry_col) {
       AFRO_region = dplyr::case_when(
         !!rlang::sym(ctry_col) %in% c("BDI","ETH","KEN","MDG","RWA","SDN","SSD","UGA","TZA") ~ "Eastern Africa",
         !!rlang::sym(ctry_col) %in% c("BWA","MOZ","MWI","NAM","SWZ","ZMB","ZWE","ZAF") ~ "Southern Africa",
-        !!rlang::sym(ctry_col) %in% c("AGO","CMR","CAF","TCD","COG","COD","GNQ","GAN") ~ "Middle Africa",
+        !!rlang::sym(ctry_col) %in% c("AGO","CMR","CAF","TCD","COG","COD","GNQ","GAN") ~ "Central Africa",
         !!rlang::sym(ctry_col) %in% c("BEN","BFA","CIV","GHA","GIN","GNB","LBR","MLI","MRT","NER","NGA","SEN","SLE","TGO") ~ "Western Africa",
         !!rlang::sym(ctry_col) %in% c("DJI","SOM","SDN") ~ "Eastern Mediterranean"
       ) 
@@ -847,7 +847,7 @@ get_AFRO_region <- function(data, ctry_col) {
 #'
 #' @examples
 get_AFRO_region_levels <- function() {
-  c("Western Africa", "Middle Africa", "Eastern Africa", "Eastern Mediterranean", "Southern Africa")
+  c("Western Africa", "Central Africa", "Eastern Africa", "Eastern Mediterranean", "Southern Africa")
 }
 
 
